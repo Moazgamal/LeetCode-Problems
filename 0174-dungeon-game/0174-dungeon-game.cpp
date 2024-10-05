8def49b510614fb2cfa83f8938e642de81d13d09
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-10-06 01:26:00 +0300

    Time: 3 ms (86.42%), Space: 11.6 MB (43%) - LeetHub

---
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    
+    int fn(int r, int c , vector<vector<int>>&dun, vector<vector<int>>&dp)
+    {
+        if(r<0 || c<0 || r>= dun.size() || c>=dun[0].size())
+            return INT_MAX/2;
+        
+        auto &ret = dp[r][c];
+        if(ret != -1)
+            return ret;
+        if(r== dun.size()-1 && c == dun[0].size()-1)
+        {
+           if(dun[r][c]>=0)
+                return ret = 1;
+            return ret =  1+(-dun[r][c]);
+        }
+        
+        int c1 =  fn(r+1,c, dun, dp);
+        int c2 = fn(r,c+1,dun,dp);
+            
+       int minicost = -dun[r][c] + min(c1,c2);
+       if(minicost >0)
+        return ret = minicost;
+        return ret = 1;
+        
+    }
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        vector<vector<int>> dp(dungeon.size(), vector<int>(dungeon[0].size(), -1));
+        fn(0,0, dungeon, dp);
+        
+        return dp[0][0];
+        
+    }
+};
