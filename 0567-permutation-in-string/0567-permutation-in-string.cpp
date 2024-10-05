fc28c68faa35d728c8b49369e6ff259e3dfe089e
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-10-05 12:03:06 +0300

    Time: 17 ms (26.57%), Space: 10.8 MB (25.36%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+
+        unordered_map<char,int>mp;
+        unordered_map<char,int>mp2;
+        for(int i = 0; i< s1.size(); i++)
+        {
+            mp[s1[i]]++;
+            mp2[s1[i]]++;
+        }
+        int j = 0;
+        for(int i = 0; i< s2.size() ; i++)
+        {
+            if(mp.count(s2[i]))
+            {
+                if(mp2[s2[i]]-1 ==0)
+                {
+                    mp2.erase(s2[i]);
+                    if(mp2.size() ==0)
+                        return true;
+                }
+                else if(mp2[s2[i]]-1>0)
+                {
+                    mp2[s2[i]]--;
+                    continue;
+                }
+                else
+                {
+                    mp2[s2[i]]--;
+                    while(j<i && s2[j]!=s2[i])
+                    {
+                        ++mp2[s2[j]];
+                        j++;
+                    }
+
+                    if(++mp2[s2[j]]==0)
+                        mp2.erase(s2[j]);
+                    j++;
+                }
+
+            }
+            else
+            {
+                while(j<i)
+                    {
+                        ++mp2[s2[j]];
+                        j++;
+                    }
+                    j++;
+            }
+        }
+        return false;
+        
+    }
+};
