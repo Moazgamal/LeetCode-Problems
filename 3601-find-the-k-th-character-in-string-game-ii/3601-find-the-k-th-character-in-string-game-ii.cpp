2c03cf54d9ffb95abb44c0cc2a93b5d196127770
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-10-07 15:28:32 +0300

    Time: 19 ms (5.5%), Space: 27.9 MB (20.29%) - LeetHub

--- 3601-find-the-k-th-character-in-string-game-ii/3601-find-the-k-th-character-in-string-game-ii.cpp
+++ 3601-find-the-k-th-character-in-string-game-ii/3601-find-the-k-th-character-in-string-game-ii.cpp
@@ -5,10 +5,10 @@ public:
         for(int op = ceil(log2(k)); op>=1; op--)
         {
             cout<<op<<" ";
-            if(k > (1LL<<op)/2)
+            if(k > (pow(2,op))/2)
             {
                 conv+= (operations[op-1]);
-                k -= ((1LL<<op)/2);
+                k -= ((pow(2,op))/2);
             }
         }
         conv=conv%26;
