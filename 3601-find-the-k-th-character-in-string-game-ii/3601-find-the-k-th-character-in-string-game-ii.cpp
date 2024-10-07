35c78d4c73bafa24c83d64b1e4354d63720a461d
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-10-07 15:28:08 +0300

    Time: 11 ms (18.72%), Space: 27.8 MB (27.77%) - LeetHub

---
+++ 3601-find-the-k-th-character-in-string-game-ii/3601-find-the-k-th-character-in-string-game-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    char kthCharacter(long long k, vector<int>& operations) {
+        int conv = 0;
+        for(int op = ceil(log2(k)); op>=1; op--)
+        {
+            cout<<op<<" ";
+            if(k > (1LL<<op)/2)
+            {
+                conv+= (operations[op-1]);
+                k -= ((1LL<<op)/2);
+            }
+        }
+        conv=conv%26;
+        return 'a'+conv;
+        
+    }
+};
