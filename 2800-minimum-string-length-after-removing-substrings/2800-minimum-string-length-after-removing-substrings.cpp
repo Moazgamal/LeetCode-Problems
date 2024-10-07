4fd26c02351623326331bbd5495ec074b5d6bd3e
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-10-07 14:23:28 +0300

    Time: 6 ms (57.32%), Space: 11.6 MB (61.24%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -2,31 +2,21 @@ class Solution {
 public:
     int minLength(string s) {
 
-        char prev = '-';
-        int cnt = 1;
         stack<char>st;
         st.push(s[0]);
         for(int i=1; i< s.size(); i++)
         {
-            
-            
             if(s[i]=='B')
             {
                 if(!st.empty()&& st.top()=='A')
-                {
                     st.pop();
-                    cnt-=2;
-                }
                 else
                     st.push(s[i]);
             }
             else if(s[i]=='D')
             {
                  if(!st.empty()&& st.top()=='C')
-                {
                     st.pop();
-                    cnt-=2;
-                }
                 else
                     st.push(s[i]);
             }
