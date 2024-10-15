a5e000d44e733a8e7fc9484d1ff7c87c7baf499b
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-10-15 12:08:09 +0300

    Time: 41 ms (100%), Space: 45.7 MB (30.43%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cs
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cs
@@ -1,20 +1,16 @@
 public class Solution {
     public long MinimumSteps(string s) {
         int sum = 0;
-        char prevchar = s[s.Length-1];
-        if(prevchar=='0')
-            sum++;
         long ans = 0;
-        for(int i = s.Length-2; i>=0; i--)
+        for(int i = s.Length-1; i>=0; i--)
         {
             if(s[i] == '1')
             {
-                if(prevchar == '0')
+                if(sum>0)
                     ans+=(sum);
             }
             else
             {
-                prevchar='0';
                 sum++;
             }
         }return ans;
