4067c55c79064a04eed476de6eea4aeb32a28256
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-10-15 12:08:55 +0300

    Time: 40 ms (16.21%), Space: 15.6 MB (45.68%) - LeetHub

--- 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -3,21 +3,16 @@ public:
     long long minimumSteps(string s) {
 
         int sum = 0;
-        char prevchar = s[s.size()-1];
-        if(prevchar=='0')
-            sum++;
         long long ans = 0;
-        for(int i = s.size()-2; i>=0; i--)
+        for(int i = s.size()-1; i>=0; i--)
         {
             if(s[i] == '1')
             {
-                if(prevchar == '0')
+                if(sum>0)
                     ans+=(sum);
-                
             }
             else
             {
-                prevchar='0';
                 sum++;
             }
         }return ans;
