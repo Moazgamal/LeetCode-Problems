584ed7975794c3bf96d399a3d6531e8a6d8cf066
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-10-15 11:57:18 +0300

    Time: 27 ms (89.47%), Space: 15.6 MB (45.68%) - LeetHub

---
+++ 3195-separate-black-and-white-balls/3195-separate-black-and-white-balls.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+
+        int sum = 0;
+        char prevchar = s[s.size()-1];
+        if(prevchar=='0')
+            sum++;
+        long long ans = 0;
+        for(int i = s.size()-2; i>=0; i--)
+        {
+            if(s[i] == '1')
+            {
+                if(prevchar == '0')
+                    ans+=(sum);
+                else
+                    prevchar= '1';
+            }
+            else
+            {
+                prevchar='0';
+                sum++;
+            }
+        }return ans;
+        
+    }
+};
