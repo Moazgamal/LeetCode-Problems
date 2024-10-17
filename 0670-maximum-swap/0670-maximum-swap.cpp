7a58e892d0c80880e49f579d93d982df438e7a64
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-10-17 14:34:19 +0300

    Time: 0 ms (100%), Space: 7.6 MB (70.34%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+
+
+        string s = to_string(num);
+        vector<int>v (s.size(),-1);
+        int maxi=s.size()-1;
+        for(int i = s.size()-2; i>=0; i--)
+        {
+            if(s[i]-'0' < s[maxi]-'0')
+            {
+                v[i]=maxi;
+            }
+            else if(s[i]-'0' > s[maxi]-'0')
+            {
+                maxi=i;
+            }
+        }
+        for(int i = 0; i< v.size(); i++)
+        {
+            if(v[i] !=-1)
+            {
+                swap(s[i], s[v[i]]);
+                break;
+            }
+        }
+        stringstream ss; ss<<s; int x = 0; ss>>x; return x;
+        
+
+        
+    }
+};
