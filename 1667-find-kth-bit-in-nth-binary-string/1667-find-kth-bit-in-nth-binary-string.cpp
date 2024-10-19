cc17aaca2fb243e4c20ca1605de6996a88f101f0
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-10-19 06:19:35 +0300

    Time: 0 ms (100%), Space: 7.2 MB (94.54%) - LeetHub

--- 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
@@ -3,15 +3,14 @@ public:
     char findKthBit(int n, int k) {
 
         int inversions = 0;
-        while(n>0)
+        while(n--)
         {
-            int m = pow(2,n)-1;
+            int m = pow(2,n+1)-1;
             if(k> m/2+1)
             {
                 inversions++;
                 k = m-k+1;
             }
-            n--;
         }
         if(inversions%2==0)
             return '0';
