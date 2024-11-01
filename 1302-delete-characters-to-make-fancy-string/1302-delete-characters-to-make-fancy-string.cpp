c0a8039fdf50b8a7ecce638d212d6bf0debc54b2
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-11-01 06:34:41 +0200

    Time: 32 ms (38.96%), Space: 42.9 MB (10.7%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
     string makeFancyString(string s) {
-        stack<pair<char,int>>st;
-        st.push({s[0],1});
-        for(int i = 1; i< s.size(); i++)
+        string x ="";
+        for(int i = 0; i< s.size(); i++)
         {
-            if(s[i] == st.top().first)
-            {
-                if(st.top().second+1 == 3)
-                    continue;
-                else
-                    st.top().second++;
-            }
-            else
-                st.push({s[i],1});
-        }
-        s.clear();
-        while(!st.empty())
-        {
-            while(st.top().second--)
-            {
-                s+=st.top().first;
-            }st.pop();
-        }reverse(s.begin(), s.end());return s;
+            
+            if(i-2>=0 && s[i] == s[i-1] && s[i] == s[i-2])
+                continue;
+            x += s[i];
+        }return x;
         
     }
 };
