fa50d28ebadca6b609aca9bd73beb731b8050064
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-11-02 03:46:59 +0200

    Time: 1473 ms (5.43%), Space: 17 MB (32.31%) - LeetHub

---
+++ 2648-number-of-ways-to-earn-points/2648-number-of-ways-to-earn-points.cpp
@@ -0,0 +1,53 @@
+class Solution {
+    int MOD  = 1000000007;
+    long long fn(int idx, vector<vector<int>>&types, int target,
+    vector<vector<int>>&dp)
+    {
+        if(target ==0)
+            return 1;
+        
+        if(target<0)
+            return 0;
+        
+        
+        auto &ret = dp[idx][target];
+        if(ret != -1)   
+            return ret;
+        
+        
+        
+        
+        long long ans = 0; 
+        int cnt =0;
+        for(int i = idx; i< types.size(); i++)
+        {
+            int x = types[i][0];
+            while(x--)
+            {
+                cnt++;
+                if(target-types[i][1]*cnt<0)
+                    break;
+                long long temp =  fn(i+1, types, target-types[i][1]*cnt, dp);
+                ans =  (ans+temp)%MOD;
+            }
+            cnt=0;
+        }
+        return ret = (int)ans;
+
+        
+    }
+public:
+    int waysToReachTarget(int target, vector<vector<int>>& types) {
+
+
+        
+        vector<vector<int>>dp(types.size()+1,vector<int>(target+1,-1));
+        
+        return (int)fn(0, types, target, dp);
+        
+
+
+
+        
+    }
+};
