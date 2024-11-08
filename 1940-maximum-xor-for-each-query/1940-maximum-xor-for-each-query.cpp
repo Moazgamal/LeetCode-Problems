4d3e7c8839477735c49bab95429aff3b8ec586b7
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-11-08 06:40:28 +0200

    Time: 1 ms (74.29%), Space: 95.4 MB (82.32%) - LeetHub

--- 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.cpp
+++ 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
-        int x = pow(2, maximumBit)-1;
+        int x = (1<<maximumBit)-1;
         vector<int>result(nums.size());
-        for(int i=0; i< nums.size(); i++)
+        result[0] = nums[0]^x;
+        for(int i=1; i< nums.size(); i++)
         {
-            if(i==0)
-                result[i] = nums[i]^x;
-            else
-            {
-                result[i] = nums[i-1]^nums[i]^x;
-                nums[i] = nums[i-1]^nums[i];
-            }
+            nums[i] = nums[i-1]^nums[i];
+            result[i] = nums[i]^x;
         }
         reverse(result.begin(), result.end());
         return result;
