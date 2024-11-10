b45dbba4132f4b2bd40ca439c247bdbd16e3c33e
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-11-10 08:33:03 +0200

    Time: 382 ms (8.33%), Space: 172.7 MB (100%) - LeetHub

--- 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.cpp
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int maxIncreasingSubarrays(vector<int>& nums) {
         int ans = 0;
+        int l1=-1;
+        int cnt = 0; 
         for(int i = 0; i< nums.size(); i++)
         {
             int j = i+1;
@@ -12,30 +14,20 @@ public:
                 else
                     break;
             }
-            if(j-i <= ans)
+           
+           int l2 = j-i;
+           
+            int v = l1!=-1?min(l1,l2):0;
+            if(l1 ==-1)
             {
-                i=j-1;
-                continue;
+                l1 = j-i;
             }
-            int m = j+1;
-            for(; m< nums.size(); m++)
-            {
-                if(nums[m]>nums[m-1])
-                    continue;
-                else
-                    break;
-            }
-            int l1 = j-i;
-            int l2 = m-j;
-            int v = min(l1,l2);
             v = max(v, l1/2);
             v = max(v, l2/2);
             cout<<l1<<" "<<l2<<"\n";
             ans=max(ans, v);
-            if(l1>l2)
-                i=m-1;
-            else
-                i = j-1;
+            i=j-1;
+            l1=l2;
             
         }
         
