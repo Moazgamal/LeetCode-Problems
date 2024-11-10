50b0c7493be196365f5fa57144047ab6d3fc1f67
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-11-10 06:40:10 +0200

    Time: 20 ms (100%), Space: 39.7 MB (100%) - LeetHub

--- 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.cpp
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.cpp
@@ -5,27 +5,38 @@ public:
         for(int i = 0; i< nums.size(); i++)
         {
             int j = i+1;
-            for(; j<i+k && j< nums.size(); j++)
+            for(;  j< nums.size(); j++)
             {
                 if(nums[j] > nums[j-1])
                     continue;
                 else
                     break;
             }
-            if(j<i+k)
+            if(j-i <k)
+            {
+                i=j-1;
                 continue;
+            }
             int m = j+1;
-            for(; m< j+k && m< nums.size(); m++)
+            for(; m< nums.size(); m++)
             {
                 if(nums[m]>nums[m-1])
                     continue;
                 else
                     break;
             }
-            if(m<j+k)
-                continue;
-            else
+           int l1 = j-i;
+            int l2 = m-j;
+            int v = min(l1,l2);
+            v = max(v, l1/2);
+            v = max(v, l2/2);
+            cout<<v<<" \n";
+            if(v>=k)
                 return true;
+            if(l1>l2)
+                i=m-1;
+            else
+                i = j-1;
             
         }return false;
     }
