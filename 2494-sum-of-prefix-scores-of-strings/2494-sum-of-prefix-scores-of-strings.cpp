dda5cad661569ea91124ddb82621b8bc40ece31b
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-11-18 15:16:37 +0200

    Time: 641 ms (56.39%), Space: 704 MB (78.97%) - LeetHub

--- 2494-sum-of-prefix-scores-of-strings/2494-sum-of-prefix-scores-of-strings.cpp
+++ 2494-sum-of-prefix-scores-of-strings/2494-sum-of-prefix-scores-of-strings.cpp
@@ -5,44 +5,44 @@ class Solution {
         const static int MAXCHAR  = 26;
         Trie* arr[MAXCHAR]{};
         int cnt{}; 
-        
-        
-    };
-    void add_word(Trie* &trie,string &str, int idx=0)
+         void add_word(string &str, int idx=0)
         {
             if(idx>=str.size())
                 return ;
             int i = str[idx]-'a';
-            if(trie->arr[i] == nullptr)
-                trie->arr[i] = new Trie();
-            trie->arr[i]->cnt++;
-            add_word(trie->arr[i],str, idx+1);
+            if(arr[i] == nullptr)
+                arr[i] = new Trie();
+            arr[i]->cnt++;
+            arr[i]->add_word(str, idx+1);
         }
-    int is_prefix(Trie* &trie,string &str, int idx= 0)
+    int is_prefix(string &str, int idx= 0)
         {
             int sum = 0; 
             if(idx>=str.size())
                 return 0;
             int i = str[idx]-'a';
             
-            if(!trie->arr[i])
+            if(!arr[i])
                 return 0;
-            sum += trie->arr[i]->cnt;
-            return sum+is_prefix(trie->arr[i],str, idx+1);
+            sum += arr[i]->cnt;
+            return sum+arr[i]->is_prefix(str, idx+1);
         }
+        
+    };
+   
 public:
     vector<int> sumPrefixScores(vector<string>& words) {
         Trie* trie = new Trie();
         for(int i = 0; i< words.size(); i++)
         {
-            add_word(trie,words[i]);
+            trie->add_word(words[i]);
         }
         vector<int>result(words.size(),0);
         for(int i = 0; i< words.size(); i++)
         {
             int cnt = 0; 
             
-                cnt += is_prefix(trie,words[i]);
+                cnt += trie->is_prefix(words[i]);
             
             result[i]= cnt;
         }return result;
