384ea5e126b0097886f261c45c529683f9bdc07a
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-11-23 11:45:54 +0200

    Time: 227 ms (43.64%), Space: 14.4 MB (78.93%) - LeetHub

---
+++ 3631-count-k-reducible-numbers-less-than-n/3631-count-k-reducible-numbers-less-than-n.cpp
@@ -0,0 +1,69 @@
+class Solution {
+    const int MOD = 1000000007;
+    int dp[801][2][801];
+    int cnt[801];
+    int solve(int idx, string &s, int &k , int tight, int setbits)
+    {
+        if(idx== s.size())
+        {
+            if(tight==1)
+                return 0;
+            if(cnt[setbits] <k)
+                return 1;
+            return 0;
+        }
+        
+        
+        if(s[idx]=='1')
+            setbits++;
+        auto &ret = dp[idx][tight][setbits];
+        if(dp[idx][tight][setbits] != -1)
+            return dp[idx][tight][setbits];
+        ret = 0;
+        if(tight ==1)
+        {
+            if(s[idx]=='1')
+                {
+                    ret = (ret+ solve(idx+1, s, k, 0, setbits-1))%MOD;
+                    ret = (ret+solve(idx+1, s, k, 1, setbits))%MOD;
+                }
+            else
+                {
+                    ret = (ret+solve(idx+1, s, k, 1, setbits))%MOD;
+                }
+        }
+        else
+        {
+            if(s[idx]=='1')
+                {
+                    ret = (ret +solve(idx+1,s,k,0,setbits-1))%MOD;
+                    ret = (ret+solve(idx+1, s, k, 0, setbits))%MOD;
+                }
+            else
+            {
+                ret = (ret+solve(idx+1, s, k, 0, setbits))%MOD;
+                ret  = (ret +solve(idx+1, s, k, 0, setbits+1))%MOD;
+            }
+        }
+        return dp[idx][tight][setbits] = ret;
+    }
+public:
+    int countKReducibleNumbers(string s, int k) {
+        
+        cnt[1]=0
+        ;
+        for(int i = 2; i<=800; i++)
+        {
+            int setbits = __builtin_popcount(i);
+            cnt[i]= 1+cnt[setbits];
+        }
+        memset(dp, -1, sizeof(dp));
+        int tight = 1; 
+        int setbits = 0;
+        int ans = solve(0, s, k, tight, setbits);
+        return ans-1;
+        
+        
+        
+    }
+};
