dc29e99d610575fc83074a17e03ed4601b9d1767
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-11-28 14:21:46 +0200

    Time: 0 ms (100%), Space: 8.1 MB (12.04%) - LeetHub

--- 1274-number-of-days-between-two-dates/1274-number-of-days-between-two-dates.cpp
+++ 1274-number-of-days-between-two-dates/1274-number-of-days-between-two-dates.cpp
@@ -1,34 +1,59 @@
-class Solution {   
-    public:
-        int daysBetweenDates(string date1, string date2) {
-            int year1 = stoi(date1.substr(0, 4));
-            int year2 =  stoi(date2.substr(0, 4));
-            int minyear = min(year1, year2);
-            return abs(date_to_int(date2,minyear) - date_to_int(date1,minyear));
-        }
-        
-    protected:
-        int date_to_int(string s_date, int minyear)
-        {
-            int Y = stoi(s_date.substr(0, 4));
-            int M = stoi(s_date.substr(5, 2));
-            int D = stoi(s_date.substr(8, 2));
-            
-            int date = 0;
-            for(int y = minyear; y < Y; ++y) date += is_leap_year(y) ? 366 : 365;
-            for(int m = 1; m < M; ++m) date += days_in_month(m, Y);
-            return date + D;
-        }
-        
-        bool is_leap_year(int year)
-        {
-            return (year %4  == 0 && year % 100 != 0) || year % 400 == 0;
-        }
-        
-        int days_in_month(int m, int year)
-        { 
-            if(m==1 || m==3 || m==5 || m==7 || m==8 || m==10 || m==12 ) return 31;
-            if(m==2) return is_leap_year(year) ? 29 : 28;
+class Solution {
+    bool isLeap(int &year)
+    {
+        if(year %100 ==0)
+            if(year %400 ==0)
+                return true;
+            else
+                return false;
+        else if(year %4 ==0)
+            return true;
+        return false;
+    }
+    int daysInMonth(int &month, int &year)
+    {
+        if(month == 2)
+            if(isLeap(year))
+                return 29;
+            else
+                return 28;
+        if(month == 4 || month == 6 || month == 9 || month == 11)
             return 30;
+        return 31;
+    }
+    int calcDays(int &minYear, int &curYear, string &Date)
+    {
+        int totalDays = 0; 
+        for(int y = minYear; y < curYear; y++)
+        {
+            if(isLeap(y))
+                totalDays += 366;
+            else
+                totalDays += 365;
         }
+        int curMonth = stoi(Date.substr(5,2));
+        for(int month = 1; month < curMonth; month++)
+        {
+            totalDays += daysInMonth(month, curYear);
+        }
+        int curDays = stoi(Date.substr(8,2));
+        totalDays += curDays;
+        return totalDays;
+    }
+public:
+    int daysBetweenDates(string date1, string date2) {
+
+        int year1 = stoi(date1.substr(0,4));
+        int year2 = stoi(date2.substr(0,4));
+        int minYear = min(year1, year2);
+
+        int Days1 = calcDays(minYear, year1, date1);
+        int Days2 = calcDays(minYear, year2, date2);
+        return abs(Days1-Days2);
+
+        
+    }
 };
+
+
+
