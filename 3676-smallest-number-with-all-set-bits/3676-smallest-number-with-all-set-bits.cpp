6112fb1bae3d75dccea7269ce76109daebd50170
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-12-01 20:33:50 +0200

    Time: 0 ms (100%), Space: 8.1 MB (6.67%) - LeetHub

--- 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
     int smallestNumber(int n) {
-        int ans = 0;
-        int i = 0; 
-        while(true)
-            {
-                ans += (1<<i);
-                i++;
-                if(ans>=n)
-                        return ans;
-            }
-        return ans; 
+        // int ans = 0;
+        // int i = 0; 
+        // while(true)
+        //     {
+        //         ans += (1<<i);
+        //         i++;
+        //         if(ans>=n)
+        //                 return ans;
+        //     }
+        // return ans; 
+        int leftMostBit = 0;
+        for(int i = 0; i<10; i++)
+        {
+           if( ((1<<i) & n) == (1<<i))
+           {
+                leftMostBit = i; 
+           }
+        }
+        cout<<leftMostBit<<"\n";
+        int no = (1<<leftMostBit);
+        no = no+ (no-1);
+        if(no >= n)
+            return no;
+        leftMostBit++;
+        no = (1<<leftMostBit); no = no+(no-1);
+        return no ;
         
     }
 };
