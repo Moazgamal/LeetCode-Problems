de91cd758bb93c94b65336c9155c0516e0bacc22
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-12-11 04:49:31 +0200

    Time: 387 ms (26.59%), Space: 300.8 MB (5.17%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
 
-        map<int,int> mp;
+        vector<int> v (3*100000+4,0);
         for(int i = 0; i< nums.size(); i++)
         {
-            mp[nums[i]-k]++;
-            mp[nums[i]+k+1]--;
+            v[nums[i]-k+100000]++;
+            v[nums[i]+k+1+100000]--;
         }
         int prefix = 0; 
         int ans = 0; 
-        for(auto it : mp)
+        for(auto it : v)
         {
-            prefix += it.second;
+            prefix += it;
             ans = max(ans, prefix);
         }return ans;
         
