c6353de49d344c42f76973f00f652e3eba03ce9d
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-12-18 04:50:26 +0200

    Time: 0 ms (100%), Space: 14.2 MB (17.75%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-        
-
-        stack<int> st;
-        for(int i = 0; i< prices.size(); i++)
+        stack<int> st; int i = 0 ;
+        while(i < prices.size())
         {
             while(!st.empty() && prices[st.top()] >= prices[i])
             {
                 prices[st.top()] -= prices[i];
                 st.pop();
             }
-            st.push(i);
+            st.push(i); i++; 
         }return prices;
     }
 };
