2158b3e3554a70460164005d400a20981a284059
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-12-26 03:14:37 +0200

    Time: 22 ms (52.52%), Space: 61.3 MB (12.05%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    int fn(int idx , vector<int>&nums, vector<vector<int>> &dp, int sum, int target)
+    {
+        if(idx >= nums.size())
+        {
+            
+            if(target == sum+1000)
+                return 1;
+            return 0; 
+        }
+        
+        auto &ret = dp[idx][sum+1000];
+        if(ret != INT_MIN/2)
+            return ret;
+        
+        int c1 = fn(idx+1, nums, dp, -nums[idx]+sum, target);
+        int c2 = fn(idx+1, nums, dp, nums[idx]+sum, target);
+        return ret = c1+c2;
+
+    }
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+
+        vector<vector<int>> dp(nums.size()+1, vector<int>(4002, INT_MIN/2));
+        return fn(0, nums ,dp, 0, target+1000);
+        
+    }
+};
