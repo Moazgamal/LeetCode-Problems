224852b9b40d9bcf82e36955ce374625883d479f
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2024-12-27 06:30:53 +0200

    Time: 0 ms (100%), Space: 45.7 MB (42.27%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
@@ -3,12 +3,13 @@ public:
     int maxScoreSightseeingPair(vector<int>& values) {
         int maxi = values[values.size()-1];
         int ans = 0; 
-        for(int i = values.size()-2; i>=0; i--)
+        for(int i = values.size()-2; i >=0 ; i--)
         {
             maxi--;
-            ans = max(ans, values[i]+maxi);
+            ans = max(ans, values[i] + maxi);
             maxi = max(maxi, values[i]);
-        }return ans;
+        }
+        return ans;
         
     }
 };
