e434911f177455374eb518afb523a6ae98642aa8
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-01-12 09:23:59 +0200

    Time: 621 ms (0%), Space: 224.5 MB (0%) - LeetHub

---
+++ 3677-maximum-amount-of-money-robot-can-earn/3677-maximum-amount-of-money-robot-can-earn.cpp
@@ -0,0 +1,37 @@
+class Solution {
+    
+    int fn(int r, int c, vector<vector<int>>&coins, vector<vector<vector<int>>>&dp, int skippedCells)
+    {
+        if(r == coins.size()-1 && c == coins[0].size()-1)
+        {
+            if(coins[r][c]<0)
+                if(skippedCells<2)
+                    return 0;
+            return coins[r][c];
+
+        }
+        auto &ret = dp[r][c][skippedCells];
+        if(ret != INT_MIN)
+            return ret;
+        int c1 = INT_MIN;
+        if(c+1< coins[0].size())
+            c1 = coins[r][c]+fn(r,c+1,coins,dp,skippedCells);
+        int c2 = INT_MIN;
+        if(r+1<coins.size())
+            c2 = coins[r][c]+fn(r+1,c,coins,dp,skippedCells);
+        if(skippedCells<2)
+        {
+            if(c+1< coins[0].size())
+                c1 = max(c1,fn(r,c+1,coins,dp,skippedCells+1));
+            if(r+1<coins.size())
+                c2 = max(c2, fn(r+1,c,coins,dp,skippedCells+1));
+        }
+        return ret = max(c1,c2);
+    }
+public:
+    int maximumAmount(vector<vector<int>>& coins) {
+        vector<vector<vector<int>>> dp(coins.size(), vector<vector<int>>(coins[0].size(),vector<int>(3,INT_MIN)));
+        return fn(0,0,coins,dp,0);
+        
+    }
+};
