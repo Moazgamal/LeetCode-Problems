07e04939ed612bd86e3190939487fbe8e00d5123
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-01-17 04:02:36 +0200

    Time: 31 ms (8.41%), Space: 275.7 MB (20.56%) - LeetHub

---
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -0,0 +1,141 @@
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived) {
+
+        if(derived.size()==1)
+            if(derived[0] == 1)
+                return false;
+            else
+                return true;
+        vector<int> f(derived.size(), -1);
+        if(derived[derived.size()-1] == 1)
+        {
+            f[f.size()-1] = 0; f[0]=1;
+           
+            for(int i = 0; i< f.size()-1; i++)
+            {
+                int x = derived[i];
+                if(x ==1)
+                {
+                    if(f[i] == 0)
+                    {
+                        f[i+1] = 1;
+                    }
+                    else
+                    {
+                        f[i+1]=0;
+                    }
+                }
+                else
+                {
+                    if(f[i] == 0)
+                    {
+                        f[i+1] = 0;
+                    }
+                    else
+                    {
+                        f[i+1]=1;
+                    }
+                }
+            }
+            if(f[f.size()-1] ==0 && f[0]==1)
+                return true;
+            f[f.size()-1] = 1; f[0]=0;
+            for(int i = 0; i< f.size()-1; i++)
+            {
+                int x = derived[i];
+                if(x ==1)
+                {
+                    if(f[i] == 0)
+                    {
+                        f[i+1] = 1;
+                    }
+                    else
+                    {
+                        f[i+1]=0;
+                    }
+                }
+                else
+                {
+                    if(f[i] == 0)
+                    {
+                        f[i+1] = 0;
+                    }
+                    else
+                    {
+                        f[i+1]=1;
+                    }
+                }
+            }
+            if(f[f.size()-1] == 1 && f[0]==0)
+                return true;
+            return false;
+        }
+        else
+        {
+            f[f.size()-1] = 0; f[0]=0;
+           
+            for(int i = 0; i< f.size()-1; i++)
+            {
+                int x = derived[i];
+                if(x ==1)
+                {
+                    if(f[i] == 0)
+                    {
+                        f[i+1] = 1;
+                    }
+                    else
+                    {
+                        f[i+1]=0;
+                    }
+                }
+                else
+                {
+                    if(f[i] == 0)
+                    {
+                        f[i+1] = 0;
+                    }
+                    else
+                    {
+                        f[i+1]=1;
+                    }
+                }
+            }
+            if(f[f.size()-1] ==0 && f[0]==0)
+                return true;
+            
+            f[f.size()-1] = 1; f[0]=1;
+            for(int i = 0; i< f.size()-1; i++)
+            {
+                int x = derived[i];
+                if(x ==1)
+                {
+                    if(f[i] == 0)
+                    {
+                        f[i+1] = 1;
+                    }
+                    else
+                    {
+                        f[i+1]=0;
+                    }
+                }
+                else
+                {
+                    if(f[i] == 0)
+                    {
+                        f[i+1] = 0;
+                    }
+                    else
+                    {
+                        f[i+1]=1;
+                    }
+                }
+            }
+            if(f[f.size()-1] == 1 && f[0]==1)
+                return true;
+            return false;
+        }return false;
+        
+        
+    }
+};
