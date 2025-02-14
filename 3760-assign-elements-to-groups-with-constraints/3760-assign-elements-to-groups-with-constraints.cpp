3a58efe4de36bae8e8e5ec8371e790059d853db5
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-02-14 23:07:05 +0200

    Time: 3318 ms (5.02%), Space: 294.3 MB (23.25%) - LeetHub

---
+++ 3760-assign-elements-to-groups-with-constraints/3760-assign-elements-to-groups-with-constraints.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> assignElements(vector<int>& groups, vector<int>& elements) {
+        long long maxi = -1;
+        for(int i = 0; i< groups.size(); i++)
+        {
+            maxi=max((long long)groups[i], maxi);
+        }
+        vector<long long> v (1e5+1,-1);
+        long long x = 1e5 + 5;
+       
+        for(int i = 0; i< elements.size(); i++)
+        {
+            int val = elements[i];
+            if(v[val]==-1)
+                v[val]= i;
+            else
+                continue;
+            for(long long j = val; (j)<=maxi; j+=val)
+            {
+                if(v[j] == -1)
+                {
+                    v[j] = i;
+                    cout<<j<<" "<<i<<"\n";
+                }
+            }
+        }
+        vector<int> res(groups.size(),-1);
+        for(int i = 0; i< groups.size(); i++)
+        {
+            res[i] = v[groups[i]];
+        }return res;
+        
+        
+        
+    }
+};
