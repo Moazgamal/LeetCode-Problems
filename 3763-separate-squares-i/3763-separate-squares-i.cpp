7b2830fc324b82686a0e2eb3118c91a77061356e
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-02-16 01:23:05 +0200

    Time: 189 ms (20%), Space: 182.4 MB (23.33%) - LeetHub

---
+++ 3763-separate-squares-i/3763-separate-squares-i.cpp
@@ -0,0 +1,51 @@
+class Solution {
+    bool static compare(vector<int> &a, vector<int>&b)
+    {
+        return a[1] < b[1];
+    }
+    double fn(double mid, vector<vector<int>>& squares)
+    {
+        double area = 0; 
+        for(int i = 0; i< squares.size(); i++)
+        {
+            if((double)squares[i][1] + (double)squares[i][2] <= mid)
+               area+=((double)squares[i][2] * (double)squares[i][2]);
+            else if((double)squares[i][1] < mid)
+            {
+                double h = mid - (double)squares[i][1];
+                area+=(h*(double)squares[i][2]);
+            }
+        }
+        return area;
+    }
+public:
+    double separateSquares(vector<vector<int>>& squares) {
+        sort(squares.begin(), squares.end(), compare);
+        double mini = 0;
+        double maxi = INT_MAX;
+        double totalArea =0; 
+        for(int i = 0; i< squares.size(); i++)
+        {
+            totalArea += ((double)squares[i][2] * (double)squares[i][2]);
+        }
+        totalArea/=2;
+        double mid = 0 ;
+        double pos = 0; 
+        while(maxi-mini>1e-5)
+        {
+            mid= mini + (maxi-mini)/2;
+            double area = fn(mid, squares);
+            if( area >= totalArea)
+            {
+                pos = mid;
+                maxi = mid;
+            }
+            else
+            {
+                mini= mid;
+            }
+        }
+        return mini;
+        
+    }
+};
