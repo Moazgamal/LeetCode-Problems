17302ce4b1ee797617e1c431e8a178ea995f4d9e
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-02-17 06:55:09 +0200

    Time: 3 ms (20.43%), Space: 9.1 MB (15.65%) - LeetHub

--- 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.cpp
+++ 1819-construct-the-lexicographically-largest-valid-sequence/1819-construct-the-lexicographically-largest-valid-sequence.cpp
@@ -38,7 +38,7 @@ class Solution {
                 if(idx+ i>=v.size() && i != 1)
                     continue ;
                 if(v[idx] != -1)
-                    return fn(idx+1, start, n, v, res, mp);
+                    return false;
                 if(i != 1 && v[idx+i] != -1)
                     continue;
                 v[idx]= i;
