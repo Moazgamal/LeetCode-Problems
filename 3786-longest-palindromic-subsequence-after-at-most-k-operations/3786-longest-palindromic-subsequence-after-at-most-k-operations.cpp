38fba474eaaaa7cfb9a3fc9f543382632271d692
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-03-03 03:35:49 +0200

    Time: 448 ms (83.33%), Space: 243.9 MB (41.67%) - LeetHub

---
+++ 3786-longest-palindromic-subsequence-after-at-most-k-operations/3786-longest-palindromic-subsequence-after-at-most-k-operations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<vector<int>>>dp;
+    int mini(char &a,char &b){
+        int diff=abs(a-b);
+        return min(diff,26-diff);
+    }
+    int solve(int i,int j,string &s,int k){
+        if(i==j) return 1;
+        if(i>j) return 0;
+        if(dp[i][j][k]!=-1) return dp[i][j][k];
+        int ans=max(solve(i+1,j,s,k),solve(i,j-1,s,k));
+        int c=mini(s[i],s[j]);
+        if(c<=k){
+            ans=max(ans,2+solve(i+1,j-1,s,k-c));
+        }
+        return dp[i][j][k]=ans;
+    }
+    int longestPalindromicSubsequence(string s, int k) {
+        int n=s.size();
+       
+        dp.resize(n,vector<vector<int>>(n+1,vector<int>(k+1,-1)));
+        return solve(0,s.size()-1,s,k);
+    }
+};
