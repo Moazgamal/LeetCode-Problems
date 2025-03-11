b61a3fd4d7735a253c4cf514a18cf6a02260bd33
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-03-11 03:20:40 +0200

    Time: 2952 ms (5.06%), Space: 56.5 MB (41.29%) - LeetHub

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
@@ -1,85 +1,77 @@
 class Solution {
-            bool isvowel(char func)
-        {
-        
-        if(func =='a')
-                return 1;
-        if(func =='e')
-                return 1;
-        if(func =='i')
-                return 1;
-        if(func =='o')
-                return 1;
-        if(func =='u')
-                return 1;
-        if(func =='A')
-                return 1;
-        if(func =='E')
-                return 1;
-        if(func =='I')
-                return 1;
-        if(func =='O')
-                return 1;
-        if(func =='U')
-                return 1;
-        else
-                return 0;
-        }
+    bool isVowel(char ch) {
+    ch = tolower(ch); // Convert to lowercase to handle uppercase letters
+    return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+}
 public:
     long long countOfSubstrings(string word, int k) {
-        
-        vector<int> indices(word.size());
-        int next = word.size();
-        for(int i = word.size()-1; i>=0; i--)
-        {
-            indices[i] = next;
-            if(!isvowel(word[i]))
-                next = i;
-        }
-        int j = 0;
+        long long ans = 0; 
         int cons = 0; 
-        unordered_map<char,int> mp;
-        long long ans = 0;
-        for(int i = 0 ; i< word.size(); i++)
+        unordered_map<char,int>mp;
+        int j = 0; 
+        vector<int> consInd(word.size());
+        consInd[consInd.size()-1]= consInd.size();
+        int lastConsIdx = consInd.size();
+        if(!isVowel(word[word.size()-1]))
+             lastConsIdx = word.size()-1;
+        
+        for(int i = word.size()-2; i>=0;i--)
         {
-            if(isvowel(word[i]))
-                {
-                    mp[word[i]]++;
-                }
+            consInd[i]= lastConsIdx;
+            if(!isVowel(word[i]))
+                lastConsIdx = i; 
+        }
+        for(int i = 0; j< word.size(); )
+        {
+            if(isVowel(word[j]))
+            {
+                mp[word[j]]++;
+            }
             else
                 cons++;
-            if(cons>k)
+            while(mp.size()==5 && cons>=k)
             {
-                while(j<=i && cons >k)
+                if(cons==k)
                 {
-                    if(isvowel(word[j]))
+                    int idx = consInd[j];
+                    cout<<idx<<"  \n";
+                    while(mp.size()==5 && cons == k)
                     {
-                        mp[word[j]]--;
-                        if(mp[word[j]] == 0)
-                            mp.erase(word[j]);
+                        ans += (idx-j);
+                        if(isVowel(word[i]))
+                        {
+                            mp[word[i]]--;
+                            if(mp[word[i]]==0)
+                                mp.erase(word[i]);
+                        }
+                        else
+                        {
+                            cons--; 
+                        }
+                        i++;
                     }
-                    else
-                        cons--;
-                    j++;
                 }
-                
-            }
-        while(j<i&&mp.size() == 5 && cons == k)
-            {
-                int next = indices[i];
-                ans += (next-i);
-                if(isvowel(word[j]))
+                else if(cons >k )
                 {
-                    if(--mp[word[j]] ==0)
-                        mp.erase(word[j]);
+                    while(cons > k)
+                    {
+                        if(isVowel(word[i]))
+                        {
+                            mp[word[i]]--;
+                            if(mp[word[i]]==0)
+                                mp.erase(word[i]);
+                        }
+                        else
+                        {
+                            cons--; 
+                        }
+                        i++;
+                    }
                 }
-                else
-                    cons--;
-                j++;
             }
-            
+            j++;
         }
-        return ans;
-
+        return ans; 
+        
     }
 };
