1976b8ca6ee9ae6fb5f796f1b5b1522e624742ab
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-03-26 09:45:32 +0200

    Time: 49 ms (36.17%), Space: 90.1 MB (48.02%) - LeetHub

---
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+        if(grid.size()==1 && grid[0].size()==1)
+            return 0; 
+        vector<int> v;
+        for(int r = 0; r< grid.size(); r++)
+        {
+            for(int c = 0; c< grid[0].size(); c++)
+            {
+                v.push_back(grid[r][c]);
+            }
+        }
+        sort(v.begin(), v.end());
+        int sz= v.size()/2 ;
+        int ans = 0; 
+        int num = v[sz];
+        cout<<num<<"\n";
+        for(int i =0; i<v.size(); i++)
+        {
+            if(abs(num-v[i])%x !=0)
+                return -1;
+            
+
+            ans+= abs(num-v[i])/x; 
+        }
+        return ans;
+
+        
+    }
+};
