d0319efa966bffca7082f498f5852283264a14b3
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-03-30 02:59:09 +0200

    Time: 3 ms (30.82%), Space: 9 MB (49.45%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.cpp
+++ 0768-partition-labels/0768-partition-labels.cpp
@@ -1,46 +1,26 @@
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        unordered_map<char,int>mp;
         unordered_map<char,int>mp2;
         for(int i =0; i< s.size(); i++)
-        {
-            if(mp.count(s[i])==0)
-            {
-                mp[s[i]]=i;
-                mp2[s[i]]=i;
-            }
-            else
-            {
-                mp2[s[i]]= i; 
-            }
-        }
-        int ans = 0; 
+            mp2[s[i]]= i; 
+            
         int maxidx=-1;
         vector<int>v;
         for(int i = 0; i< s.size(); i++)
         {
             if(i > maxidx)
-            {
-                ans++;
                 v.push_back(i);
-                
-            }
-            maxidx = max(maxidx, mp2[s[i]]);
 
+            maxidx = max(maxidx, mp2[s[i]]);
         }
         vector<int>res;
         for(int i = 0; i< v.size(); i++)
         {
-
             if(i == v.size()-1)
-            {
                 res.push_back(s.size() - v[i]);
-            }
             else
-            {
                 res.push_back(v[i+1]-v[i]);
-            }
         }
         return res; 
 
