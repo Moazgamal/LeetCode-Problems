af489e770442f165da8f46a4c5be3c16dd7c8998
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-04-26 10:59:12 +0300

    Time: 4 ms (38.42%), Space: 93.8 MB (94.46%) - LeetHub

---
+++ 2527-count-subarrays-with-fixed-bounds/2527-count-subarrays-with-fixed-bounds.cpp
@@ -0,0 +1,78 @@
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int minK, int maxK) {
+        long long ans = 0; 
+        int j = -1;
+        int lastmin = -1;
+        int lastmax = -1; 
+        for(int i = 0; i< nums.size(); i++)
+        {
+            if(nums[i] < minK || nums[i] > maxK)
+            {
+                j = i ;
+                lastmin = -1; 
+                lastmax = -1; 
+                continue;
+            }
+            if(nums[i] == minK && nums[i] == maxK)
+            {
+                if(j == -1)
+                {
+                    ans+= (i+1);
+                }
+                else
+                    ans+= (i-j);
+                lastmin= i;
+                lastmax=i;
+                continue;
+            }
+            
+            if(nums[i] != minK && nums[i] != maxK)
+            {
+                if(lastmin != -1 && lastmax != -1)
+                {
+                    int idx = min(lastmin, lastmax);
+                    if(j == -1)
+                    {
+                        ans+= (idx+1);
+                    }
+                    else
+                    {
+                        ans+=(idx-j);
+                    }
+                }
+            }
+            else if(nums[i] == minK  )
+            {
+                if(lastmax != -1)
+                {
+                        if(j == -1)
+                    {
+                        ans+= (lastmax+1);
+                    }
+                    else
+                    {
+                        ans+=(lastmax-j);
+                    }
+                }
+                lastmin = i;
+            }
+            else if(nums[i] == maxK )
+            {
+                if(lastmin != -1 )
+                {
+                        if(j == -1)
+                    {
+                        ans+= (lastmin+1);
+                    }
+                    else
+                    {
+                        ans+=(lastmin-j);
+                    }
+                }
+                lastmax = i;
+            }
+        }return ans;
+        
+    }
+};
