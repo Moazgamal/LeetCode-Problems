28c1a1b5f3b55ad7184797b7e01259ba7292c32c
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-04-30 03:18:32 +0300

    Time: 4 ms (10.27%), Space: 58.2 MB (13.01%) - LeetHub

---
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.ts
@@ -0,0 +1,18 @@
+function findNumbers(nums: number[]): number {
+
+    let ans = 0; 
+        for(let i = 0; i< nums.length; i++)
+        {
+            let cnt = 0;
+           
+            while(Math.floor(nums[i]) > 0)
+            {
+                nums[i] = nums[i]/ 10; 
+                cnt++;
+            }
+            
+            if(cnt %2 ==0)
+                ans++;
+        }return ans;
+    
+};
