a6fc89e98f784cb551e429e88dd5167fb6fc6f83
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-05-02 08:17:05 +0300

    Time: 4 ms (91.43%), Space: 18.2 MB (71.07%) - LeetHub

--- 0868-push-dominoes/0868-push-dominoes.cpp
+++ 0868-push-dominoes/0868-push-dominoes.cpp
@@ -1,67 +1,90 @@
 class Solution {
-    string fn(string &dom)
-    {
-        string ans = "";
-        for(int i = 0; i< dom.size(); i++)
-        {
-            if(dom[i] == '.')
-            {
-                if(i-1 >= 0 && i+1 < dom.size())
-                {
-                    if(dom[i-1] == 'R' && dom[i+1] == 'L')
-                    {
-                        ans.push_back('.');
-                    }
-                    else if(dom[i-1] == 'R')
-                    {
-                        ans.push_back('R');
-                    }
-                    else if(dom[i+1] == 'L')
-                    {
-                        ans.push_back('L');
-                    }
-                    else
-                    {
-                        ans.push_back('.');
-                    }
-                }
-                else if(i-1>=0)
-                {
-                    if(dom[i-1] == 'R')
-                    {
-                        ans.push_back('R');
-                    }
-                    else
-                    {
-                        ans.push_back('.');
-                    }
-                }
-                else if(i+1 < dom.size())
-                {
-                    if(dom[i+1] == 'L')
-                        ans.push_back('L');
-                    else
-                        ans.push_back('.');
-                }
-                else
-                    ans.push_back('.');
-            }
-            else
-                ans.push_back(dom[i]);
-        }return ans; 
-    }
 public:
     string pushDominoes(string dominoes) {
         string ans = "";
-       
-       while(true)
-       {
-            string x = fn(dominoes);
-            if(x == ans)
-                return x;
-            ans = x; 
-            dominoes = x;
-       }return ans; 
+        char prev = '$';
+        for(int i = 0; i< dominoes.size(); )
+        {
+            if(dominoes[i]=='L' || dominoes[i]=='R')
+            {
+                ans.push_back(dominoes[i]);
+                prev = dominoes[i];
+                i++;
+            }
+            else
+            {
+                if(prev == '$')
+                {
+                    int cnt = 0; 
+                    while(i<dominoes.size() && dominoes[i]=='.')
+                    {
+                        cnt++;
+                        i++;
+                    }
+                    if(i<dominoes.size())
+                    {
+                        if(dominoes[i] == 'L')
+                            ans.append(cnt, 'L');
+                        else
+                            ans.append(cnt, '.');
+                    }
+                    else
+                        ans.append(cnt, '.');
+                    
+                }
+                else
+                {
+                    int cnt = 0; 
+                    while(i<dominoes.size() && dominoes[i]=='.')
+                    {
+                        cnt++;
+                        i++;
+                    }
+                    if(i== dominoes.size())
+                    {
+                        if(prev == 'R')
+                            ans.append(cnt, 'R');
+                        else
+                            ans.append(cnt, '.');
+                    }
+                    else
+                    {
+                        if(prev== 'R')
+                        {
+                            if(dominoes[i] == 'R')
+                                ans.append(cnt, 'R');
+                            else
+                            {
+                                if(cnt %2 == 0)
+                                {
+                                    ans.append(cnt/2, 'R');
+                                    ans.append(cnt/2, 'L');
+                                }
+                                else
+                                {
+                                    ans.append(cnt/2, 'R');
+                                    ans.push_back('.');
+                                    ans.append(cnt/2, 'L');
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if(prev == 'L')
+                            {
+                                if(dominoes[i] == 'L')
+                                    ans.append(cnt, 'L');
+                                else
+                                {
+                                    ans.append(cnt, '.');
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return ans; 
         
     }
 };
