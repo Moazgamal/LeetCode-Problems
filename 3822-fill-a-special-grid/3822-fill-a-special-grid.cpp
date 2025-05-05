b6e52538a4478a4c7ec0a0ea7a650b5dbf852f98
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-05-05 08:33:33 +0300

    Time: 7 ms (92.31%), Space: 66.2 MB (23.08%) - LeetHub

---
+++ 3822-fill-a-special-grid/3822-fill-a-special-grid.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    void build(int r, int cmax, long long N, int &cnt, vector<vector<int>> &ans)
+    {
+        if(N == 2)
+        {
+            ans[r][cmax] = cnt++;
+            ans[r+1][cmax] = cnt++;
+            ans[r+1][cmax-1] = cnt++;
+            ans[r][cmax-1] = cnt++;
+            return ;
+        }
+        long long x = N/2;
+        // x = sqrt(x);
+        for(int i = 0; i< 4; i++)
+        {
+            build(r, cmax, x, cnt, ans);
+            if(i== 0)
+                r+= x;
+            if(i==1)
+                cmax -= x;
+            if(i==2)
+                r -= x;
+            
+        }
+    }
+public:
+    vector<vector<int>> specialGrid(int N) {
+        if(N ==0)
+            return {{0}};
+        long long sz = pow(2, N);
+        vector<vector<int>> ans(sz, vector<int>(sz,0));
+        int cnt = 0; 
+        build(0, sz-1, sz, cnt, ans);
+        return ans;
+        
+        
+    }
+};
