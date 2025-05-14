b28855c3b658a5efaa169ab4b9f5e22f68161733
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-05-14 17:24:15 +0300

    Time: 7 ms (28.76%), Space: 59.5 MB (5.58%) - LeetHub

---
+++ 3871-minimum-deletions-for-at-most-k-distinct-characters/3871-minimum-deletions-for-at-most-k-distinct-characters.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var minDeletion = function(s, k) {
+
+    let mp = {};
+        for(let i = 0; i< s.length; i++)
+            {
+                mp[s[i]] = (mp[s[i]] || 0) +1;
+            }
+        let sz = Object.keys(mp).length;
+        if(sz <=k)
+            return 0; 
+       
+        let v = [];
+        for(let [key, value] of Object.entries(mp))
+            {
+                v.push(value);
+            }
+        v.sort((a, b) => a - b);
+        let ops = 0; 
+        let sz2 = v.length;
+        for(let i = 0; i< sz2; i++)
+            {
+                ops += v[i];
+                sz--;
+                if(sz ==k)
+                    return ops;
+            }
+        return ops;
+    
+};
