d9603a5ae2a986904cd03837455d792c0a4c9968
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-05-14 16:59:49 +0300

    Time: 10 ms (15.38%), Space: 59 MB (36.54%) - LeetHub

---
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.ts
@@ -0,0 +1,25 @@
+function maxFreqSum(s: string): number {
+    let mp1 = {};
+        let mp2 = {};
+        let freq1 = 0; let freq2 = 0; 
+        
+        for(let i = 0; i< s.length; i++)
+            {
+                if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+                {
+                    
+                        mp1[s[i]] = (mp1[s[i]] || 0)+1; 
+                    
+                    freq1 = Math.max(freq1, mp1[s[i]]);
+                }
+                else
+                {
+                    
+                        mp2[s[i]] = (mp2[s[i]] || 0)+1;
+                   
+                    freq2 = Math.max(freq2, mp2[s[i]]);
+                }
+            }
+        return freq1 + freq2;
+    
+};
