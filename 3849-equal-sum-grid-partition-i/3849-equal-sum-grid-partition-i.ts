380e13e2d0ee7012a9cd121b8bb0b2a4d8f7884a
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-05-14 17:33:35 +0300

    Time: 30 ms (72.97%), Space: 87.7 MB (43.24%) - LeetHub

---
+++ 3849-equal-sum-grid-partition-i/3849-equal-sum-grid-partition-i.ts
@@ -0,0 +1,42 @@
+function canPartitionGrid(grid: number[][]): boolean {
+
+    let rows ={};
+        let cols = {};
+        let tot = 0; 
+        for(let r = 0; r< grid.length; r++)
+            {
+                let sum = 0; 
+                for(let c = 0; c< grid[0].length; c++)
+                    {
+                        sum += grid[r][c];
+                    }
+                rows[r] = sum;
+                tot += sum;
+            }
+        for(let c = 0; c< grid[0].length; c++)
+            {
+                let sum = 0; 
+                for(let r = 0; r< grid.length; r++)
+                    {
+                        sum += grid[r][c];
+                    }
+                cols[c] = sum;
+            }
+       
+         let sum1 = 0; 
+        for(let i = 0; i< grid.length-1; i++)
+            {
+                sum1 += rows[i];
+                if(tot-sum1 == sum1)
+                    return true;
+            }
+        sum1 = 0; 
+        for(let i = 0; i< grid[0].length-1; i++)
+            {
+                sum1 += cols[i];
+                if(tot-sum1 == sum1)
+                    return true;
+            }
+        return false;
+    
+};
