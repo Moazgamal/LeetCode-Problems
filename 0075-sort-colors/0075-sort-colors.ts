d2fab9ece7240561a35325d1689391353885fb61
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-05-17 10:38:00 +0300

    Time: 0 ms (100%), Space: 55.4 MB (68.4%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.ts
@@ -0,0 +1,24 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function sortColors(nums: number[]): void {
+
+    let cnt1 = 0; let cnt2 = 0; let cnt3= 0; 
+        for(let i = 0; i< nums.length; i++)
+        {
+            if(nums[i] == 0)
+                cnt1++;
+            else if(nums[i] == 1)
+                cnt2++;
+            else
+                cnt3++;
+        }let j = 0; 
+       
+            while(cnt1-- >0)
+                nums[j++]= 0;
+            while(cnt2-- >0)
+                nums[j++] = 1; 
+            while(cnt3-->0)
+                nums[j++] = 2;
+    
+};
