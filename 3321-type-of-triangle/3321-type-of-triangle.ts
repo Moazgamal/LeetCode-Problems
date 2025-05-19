9c3bcbc2aa669993759a1df9e3c24d142fb35ac6
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-05-19 19:49:49 +0300

    Time: 0 ms (100%), Space: 56.2 MB (91.18%) - LeetHub

---
+++ 3321-type-of-triangle/3321-type-of-triangle.ts
@@ -0,0 +1,14 @@
+function triangleType(nums: number[]): string {
+    let n1 = nums[0];
+        let n2 = nums[1];
+        let n3 = nums[2];
+        if(n1 + n2 <= n3 || n1+n3 <= n2 || n2+n3<=n1)
+            return "none";
+        if(n1 == n2  && n2== n3)
+            return "equilateral";
+        if(n1 == n2 || n1 == n3 || n2 == n3)
+            return "isosceles";
+        return "scalene";
+        
+    
+};
