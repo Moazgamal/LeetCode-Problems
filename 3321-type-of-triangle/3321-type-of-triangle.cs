7ce056773be5a59d78b64010d601d00f8cdc7e03
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-05-19 19:48:51 +0300

    Time: 0 ms (100%), Space: 45 MB (15.63%) - LeetHub

---
+++ 3321-type-of-triangle/3321-type-of-triangle.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public string TriangleType(int[] nums) {
+
+        int n1 = nums[0];
+        int n2 = nums[1];
+        int n3 = nums[2];
+        if(n1 + n2 <= n3 || n1+n3 <= n2 || n2+n3<=n1)
+            return "none";
+        if(n1 == n2  && n2== n3)
+            return "equilateral";
+        if(n1 == n2 || n1 == n3 || n2 == n3)
+            return "isosceles";
+        return "scalene";
+        
+    }
+}
