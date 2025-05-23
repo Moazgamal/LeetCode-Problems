5425f505c1ab835d9613a66d2ed41ac38493f7c4
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-05-23 23:45:10 +0300

    Time: 4 ms (89.66%), Space: 75.8 MB (72.41%) - LeetHub

---
+++ 1206-corporate-flight-bookings/1206-corporate-flight-bookings.ts
@@ -0,0 +1,19 @@
+function corpFlightBookings(bookings: number[][], n: number): number[] {
+
+    let acc = 0; 
+        let x= new Array(n+2).fill(0);
+        for(let i = 0; i< bookings.length; i++)
+        {
+            let s = bookings[i][0];
+            let e = bookings[i][1];
+            let val = bookings[i][2];
+            x[s]+= val;
+            x[e+1]-=val;
+        }let res = new Array(n).fill(0);
+        for(let i= 0; i<n; i++)
+        {
+            acc += x[i+1];
+            res[i] = acc;
+        }return res; 
+    
+};
