8ac539a4044912b0765482a15df9be0c69b6a92d
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-05-23 23:07:14 +0300

    Time: 218 ms (45.25%), Space: 190.6 MB (19.62%) - LeetHub

--- 3795-zero-array-transformation-iv/3795-zero-array-transformation-iv.cpp
+++ 3795-zero-array-transformation-iv/3795-zero-array-transformation-iv.cpp
@@ -2,15 +2,14 @@ class Solution {
     int fn(int idx, vector<vector<int>>& q, int target, vector<vector<int>> &dp, int j)
     {
         if(target == 0)
-            return  j; 
+            return j; 
         if(target < 0)
-            return q.size()+1;
+            return  q.size()+1;
         if(j >= q.size())
             return  q.size()+1; 
         auto &ret  = dp[target][j];
         if(ret != -1)
             return ret; 
-    
        ret = fn(idx, q, target, dp, j+1); 
        if(q[j][0] <= idx && q[j][1] >= idx)
              ret = min(ret, fn(idx, q, target-q[j][2], dp,j+1));
