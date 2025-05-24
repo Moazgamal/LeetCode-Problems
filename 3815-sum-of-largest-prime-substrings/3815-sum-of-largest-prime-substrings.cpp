c3c2da4d7bbb46ea3159601e27acd162328174ea
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-05-24 19:36:03 +0300

    Time: 19 ms (23.42%), Space: 14.7 MB (5.18%) - LeetHub

---
+++ 3815-sum-of-largest-prime-substrings/3815-sum-of-largest-prime-substrings.cpp
@@ -0,0 +1,52 @@
+class Solution {
+    bool isPrime(long long n)
+    {
+        if (n < 2) return false;
+    if (n == 2 || n == 3) return true;
+    if (n % 2 == 0 || n % 3 == 0) return false;
+
+    for (long long i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+    }
+
+    return true;
+    }
+public:
+    long long sumOfLargestPrimes(string s) {
+
+        long long ans = 0; 
+        long long x1 = 0; 
+        long long x2 = 0; 
+        long long x3 = 0; 
+        unordered_map<string,bool>mp;
+        set<long long, greater<long long>> st;
+        for(int i = 0; i< s.size(); i++)
+            {
+        string str = "";
+                for(int j = i; j< s.size(); j++)
+                    {
+                        str.push_back(s[j]);
+                        if(mp.count(str)==0)
+                        {
+                            long long num = stoll(str);
+                            if(isPrime(num))
+                            {
+                                st.insert(num);
+                            }
+                            mp[str]= true;
+                        }
+                    }
+            }
+        int cnt   =0; 
+        for(auto x: st)
+            {
+                ans+= x;
+                cnt++;
+                if(cnt ==3)
+                    return ans;
+            }
+        return ans; 
+        
+    }
+};
