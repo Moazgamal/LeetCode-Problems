3ed62051c6cd0c593ac843dce705292f3ca48882
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-06-02 01:30:47 +0300

    Time: 98 ms (83.33%), Space: 58.7 MB (33.33%) - LeetHub

---
+++ 3201-distribute-candies-among-children-ii/3201-distribute-candies-among-children-ii.ts
@@ -0,0 +1,38 @@
+function distributeCandies(n: number, limit: number): number {
+    
+    let j = n - (2*limit);
+       let x1 = 0; 
+       let x2 = 0; 
+       if(j <0)
+       {
+            x1 = 0; 
+            x2 = Math.min(n, limit);
+       }
+       else
+       {
+            x1 = j;
+            x2 = Math.min(n, limit);
+       }
+        let  ans = 0; 
+       for(let k = x1 ; k<=x2 ; k++)
+       {
+            let cand = n;
+            cand -= k;
+            let x3 = 0; 
+            let x4 = 0; 
+            let u = cand- (limit);
+            if(u < 0)
+            {
+                x3 = 0;
+                x4 = Math.min(cand, limit);
+            }
+            else
+            {
+                x3 = u;
+                x4 = Math.min(cand, limit);
+            }
+            let num = (x4-x3)+1;
+            ans += num;
+       }
+       return ans; 
+};
