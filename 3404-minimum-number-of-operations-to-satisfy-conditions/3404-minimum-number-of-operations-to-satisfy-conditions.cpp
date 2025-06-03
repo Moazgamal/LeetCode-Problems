9c7dbc8202c552bf0f8b20f0ca1b03a7da2efe10
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-06-03 15:33:52 +0300

    Time: 31 ms (66.06%), Space: 119.5 MB (39.39%) - LeetHub

---
+++ 3404-minimum-number-of-operations-to-satisfy-conditions/3404-minimum-number-of-operations-to-satisfy-conditions.cpp
@@ -0,0 +1,42 @@
+class Solution {
+    int fn(int idx, vector<vector<int>>&dp, vector<vector<int>>&freqs, int rows, int forb,
+    int cols)
+    {
+        if(idx == cols)
+            return 0; 
+        auto &ret = dp[idx][forb];
+        if(ret != -1)
+            return ret; 
+        ret = INT_MAX;
+        for(int i = 0; i <=9; i++)
+        {
+            if(i == forb )
+                continue;
+            int freq = freqs[idx][i];
+            ret = min(ret, (rows-freq) + fn(idx+1, dp, freqs, rows, i,cols));
+        }
+        return ret; 
+    }
+public:
+    int minimumOperations(vector<vector<int>>& grid) {
+
+        vector<vector<int>>dp(grid[0].size(), vector<int>(11,-1));
+
+        vector<vector<int>> v(grid[0].size(), vector<int>(11,0));
+        
+        for(int c = 0; c< grid[0].size(); c++)
+        {
+            for(int r= 0; r< grid.size(); r++)
+            {
+                v[c][grid[r][c]]++;
+            }
+        }
+        return fn(0,dp,v,grid.size(),10,grid[0].size());
+
+
+
+
+
+        
+    }
+};
