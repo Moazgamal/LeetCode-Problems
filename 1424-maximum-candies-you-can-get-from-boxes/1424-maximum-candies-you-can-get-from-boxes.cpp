71bfb17e176a033d3e65709fc3b217e671e6bc1e
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-06-03 18:15:06 +0300

    Time: 7 ms (44.71%), Space: 44.5 MB (50.48%) - LeetHub

---
+++ 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
+
+        int ans = 0; 
+        queue<int>q;
+        vector<bool>reachable(status.size(), false);
+        vector<bool>vis(status.size(), false);
+        for(int i = 0; i< initialBoxes.size(); i++)
+        {
+            if(status[initialBoxes[i]] == 1)
+            {
+                q.push(initialBoxes[i]);
+                vis[initialBoxes[i]]= true;
+            }
+            else
+                reachable[initialBoxes[i]]= true;
+        }
+        unordered_set<int> k;
+        for(int sz = q.size(); !q.empty(); sz= q.size())
+        {
+            while(sz--)
+            {
+                auto cur = q.front(); q.pop();
+                ans+= candies[cur];
+                for(int i = 0; i< containedBoxes[cur].size(); i++)
+                {
+                    int box = containedBoxes[cur][i];
+                    
+                    if(!vis[box])
+                    {
+                        if(status[box] == 1)
+                        {
+                            q.push(box);
+                            vis[box] = true;
+                        }
+                        else if(k.find(box) != k.end())
+                        {
+                            q.push(box); vis[box]= true;
+                        }
+                        else
+                            reachable[box] = true;
+                    }
+                }
+                for(int j = 0; j< keys[cur].size(); j++)
+                {
+                    int box = keys[cur][j];
+                    if(!vis[box])
+                    {
+                        if(reachable[box])
+                        {
+                            q.push(box); vis[box]= true;
+                        }
+                        else
+                            k.insert(box);
+                    }
+                }
+            }
+        }
+        return ans; 
+        
+    }
+};
