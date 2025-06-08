ea6df5829ab29f315e0e31a2eba37900713d8de6
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-06-08 03:17:39 +0300

    Time: 287 ms (19.94%), Space: 105 MB (18.26%) - LeetHub

---
+++ 3445-lexicographically-minimum-string-after-removing-stars/3445-lexicographically-minimum-string-after-removing-stars.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    string clearStars(string s) {
+        vector<vector<int>> mp(26);
+        unordered_set<int> st;
+        for(int i = 0; i< s.size(); i++)
+        {
+            if(s[i] != '*')
+            {
+                mp[s[i]-'a'].push_back(i); 
+            }
+            else
+            {
+                if(mp.size() ==0)
+                    continue;
+                bool f = true; 
+                for(int x= 0; x< mp.size(); x++)
+                {
+                    if(f && mp[x].size()>0)
+                    {
+                        st.insert(mp[x][mp[x].size()-1]);
+                        mp[x].pop_back();
+                        
+                        break;
+                    }
+                }
+            }
+        }
+            string ans = "";
+            for(int i = 0; i< s.size(); i++)
+            {
+                
+                    if(s[i] != '*' && st.find(i) == st.end())
+                    {
+                        ans.push_back(s[i]); 
+                    }
+            }
+            return ans; 
+            
+        
+    }
+};
