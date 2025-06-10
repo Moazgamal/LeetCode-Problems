7c1b1ecef3b06eb574c9a664249112e55bdc7434
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-06-10 22:31:52 +0300

    Time: 0 ms (100%), Space: 29.5 MB (44.44%) - LeetHub

---
+++ 2850-construct-the-longest-new-string/2850-construct-the-longest-new-string.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int LongestString(int x, int y, int z) {
+        if(y>x)
+        {
+            return 2*(x+ (x+1) +z);
+        }
+        else if(x>y)
+        {
+            return 2*(y+ (y+1)+z);
+        }
+        return 2*(y+x+z);
+        
+    }
+}
