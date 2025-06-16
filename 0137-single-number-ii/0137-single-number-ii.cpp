ef00a02bc713007744e92901e17539f76c40e0e6
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-06-16 03:26:08 +0300

    Time: 0 ms (100%), Space: 13.2 MB (98.8%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -5,16 +5,17 @@ public:
         for(int i = 0; i< 32; i++)
         {
             int cnt = 0; 
+            int x = (1<<i);
             for(int j = 0; j< nums.size(); j++)
             {
-                if(((1<<i) & nums[j]) != 0)
+                if((x & nums[j]) != 0)
                 {
                     cnt++;
                 }
             }
             if(cnt %3 !=0)
             {
-                ans+= (1<<i);
+                ans+= (x);
             }
         }return ans; 
     }
