ce5198cc50e052d7e866c5551cb22d5981633a41
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-07-08 22:19:22 +0300

    Time: 7 ms (61.68%), Space: 69 MB (83.67%) - LeetHub

---
+++ 3934-coupon-code-validator/3934-coupon-code-validator.cpp
@@ -0,0 +1,46 @@
+class Solution {
+    bool isvalid(string &s)
+    {
+        for(int i = 0; i< s.size(); i++)
+        {
+            if(isalnum(s[i]) || s[i] == '_')
+                continue;
+            return false;
+        }
+        return true;
+    }
+public:
+    vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
+
+        vector<string>answer;
+        map<string,vector<string>>mp;
+        for(int i = 0; i< code.size(); i++)
+        {
+            if(code[i] == "")
+                continue;
+            if(businessLine[i] == "electronics"|| 
+            businessLine[i] == "grocery"||
+            businessLine[i] == "pharmacy"||
+            businessLine[i]=="restaurant")
+            {
+                if(isActive[i] == true)
+                {
+                    if(isvalid(code[i]))
+                    {
+                        mp[businessLine[i]].push_back(code[i]);
+                    }
+                }
+            }
+        }
+        for(auto x: mp)
+        {
+            sort(x.second.begin(), x.second.end());
+            for(int i= 0; i< x.second.size(); i++)
+            {
+                answer.push_back(x.second[i]);
+            }
+        }return answer;
+        
+        
+    }
+};
