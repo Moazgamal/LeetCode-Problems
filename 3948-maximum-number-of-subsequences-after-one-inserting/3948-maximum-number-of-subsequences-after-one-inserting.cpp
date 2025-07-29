1717bcd992781c54fbf252b8bdc4f88936189919
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-07-29 18:57:44 +0300

    Time: 17 ms (88.56%), Space: 24.5 MB (82.28%) - LeetHub

---
+++ 3948-maximum-number-of-subsequences-after-one-inserting/3948-maximum-number-of-subsequences-after-one-inserting.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    long long numOfSubsequences(string s) {
+        int acc =0;
+        vector<int>v(s.size(),0);
+        for(int i = 0; i< s.size(); i++)
+        {
+            if(s[i] == 'L')
+            {
+                acc++;
+            }
+            v[i]= acc;
+        }
+
+        long long totalSum=0;
+        long long totalT=0;
+        long long totalC = 0; 
+        long long Extra = 0; 
+        long long ans = 0; 
+        long long val = 0; 
+        for(int i = (int)s.size()-1; i>=0; i--)
+        {
+            if(s[i] == 'T')
+            {
+                totalT++; continue;
+            }
+            else if(s[i] == 'C')
+            {
+                totalC++;
+                totalSum += totalT;
+            }
+            else if(s[i] == 'L')
+            {
+                val += totalC;
+                ans += totalSum;
+                Extra = max(Extra, max(totalT*v[i], totalC*v[i]));
+            }
+        }
+        Extra = max(Extra, totalSum);
+        Extra= max(Extra, val);
+        
+        return ans + Extra;
+
+        
+    }
+};
