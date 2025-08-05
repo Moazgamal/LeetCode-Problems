e3bfcee2c30e0dc56a82c2f0e413bf153c0727d4
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-08-05 16:02:50 +0300

    Time: 0 ms (100%), Space: 153.8 MB (42.85%) - LeetHub

--- 3954-maximum-balanced-shipments/3954-maximum-balanced-shipments.cpp
+++ 3954-maximum-balanced-shipments/3954-maximum-balanced-shipments.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
     int maxBalancedShipments(vector<int>& weight) {
-        int n = weight.size();
-        int r = 0;
+
         int res = 0;
-        stack<int> st;
-        while (r < n) 
-        {
-            while (!st.empty() && weight[st.top()] < weight[r])
-                st.pop();
-            if (!st.empty() && weight[r] < weight[st.top()]) 
-            {
-                res++;
-                while (!st.empty()) st.pop();
-                r++;
-                continue;
-            }
-            st.push(r);
-            r++;
+    for (int i = 1; i < weight.size(); ++i)
+        if (weight[i - 1] > weight[i]) {
+            ++res;
+            ++i;
         }
-        return res;
+    return res;
         
     }
 };
