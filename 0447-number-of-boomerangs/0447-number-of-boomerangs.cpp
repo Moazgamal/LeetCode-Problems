719817be1a139ffda8ec5691613322fd31cb433d
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-08-09 17:34:09 +0300

    Time: 675 ms (11.72%), Space: 180.3 MB (6.93%) - LeetHub

---
+++ 0447-number-of-boomerangs/0447-number-of-boomerangs.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int numberOfBoomerangs(vector<vector<int>>& points) {
+
+        double ans = 0; 
+        vector<vector<double>>v(points.size(), vector<double>(points.size(),-1));
+        vector<unordered_map<double,int>>vv(points.size());
+        for(int i = 0; i< points.size()-1; i++)
+        {
+            for(int  j = i+1; j< points.size(); j++)
+            {
+                double dis = sqrt(
+                (abs(points[i][0]-points[j][0]))*(abs(points[i][0]-points[j][0]))
+                +
+                (abs(points[i][1]-points[j][1]))*(abs(points[i][1]-points[j][1]))
+                );
+                v[i][j] = dis; v[j][i]= dis;
+                vv[i][dis]++;
+                vv[j][dis]++;
+            }
+        }
+
+        for(int i = 0; i< points.size(); i++)
+        {
+            for(int j = 0; j< points.size(); j++)
+            {
+                if(i == j)
+                    continue;
+                double dis1 = v[i][j];
+                int num1 = vv[j][dis1]-1;
+                if(num1>0)
+                    ans+= num1;
+            }
+            
+        }
+        return ans;
+        
+    }
+};
