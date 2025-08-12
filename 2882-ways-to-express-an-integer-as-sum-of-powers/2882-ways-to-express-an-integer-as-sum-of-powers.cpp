04e7770c153eb0e9e5f4c63c3b7c177c257c14d0
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-08-12 09:21:08 +0300

    Time: 750 ms (10.67%), Space: 9.5 MB (97.6%) - LeetHub

--- 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
+++ 2882-ways-to-express-an-integer-as-sum-of-powers/2882-ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -1,19 +1,54 @@
 class Solution {
     const int MOD= 1e9+7;
     int dp[301][301];
+    int roots[301][6]; // roots[num][n]
+
+void precomputeRoots() {
+    for (int num = 1; num <= 300; num++) {
+        for (int n = 1; n <= 5; n++) {
+            roots[num][n] = (int)round(pow(num, 1.0 / n));
+        }
+    }
+}
+
+inline int getRoot(int num, int n) {
+    return roots[num][n];
+}
+    inline long long fastPowSmall(int base, int exp) {
+    switch (exp) {
+        case 1: return base;
+        case 2: return base * base;
+        case 3: return base * base * base;
+        case 4: {
+            long long sq = base * base;
+            return sq * sq;
+        }
+        case 5: {
+            long long sq = base * base;
+            return sq * sq * base;
+        }
+        default: return 1; // shouldn't happen
+    }
+}
+    
     int fn(int sum, int p, int cur)
     {
         if(sum ==0)
         {
             return 1; 
         }
-        auto &ret = dp[sum][cur];
+        auto ret = dp[sum][cur];
         if(ret != -1)
             return ret;
         int ans = 0; 
-        for(int i = cur; i<= sum; i++)
+        int ro = sum;
+
+        
+        ro = ceil(getRoot(sum,p));
+        
+        for(int i = cur; i<= ro; i++)
         {
-            long long num = pow(i,p);
+            long long num = fastPowSmall(i,p);
             if(num<=sum)
             {
                 ans+= fn(sum-num, p, i+1);
@@ -25,9 +60,9 @@ class Solution {
     
 public:
     int numberOfWays(int n, int x) {
-        // vector<vector<int>>dp(301, vector<int>(301, -1));
+        precomputeRoots();
         memset(dp, -1, sizeof(dp));
-        return fn(n, x, 1)%MOD ;
+        return fn(n, x, 1) ;
         
     }
 };
