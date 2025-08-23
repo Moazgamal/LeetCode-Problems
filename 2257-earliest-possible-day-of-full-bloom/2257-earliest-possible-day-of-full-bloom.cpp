d8327a1fe36fd29b5935171a1ced472b522ed42f
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-08-23 15:07:32 +0300

    Time: 99 ms (21.78%), Space: 180.3 MB (12.87%) - LeetHub

---
+++ 2257-earliest-possible-day-of-full-bloom/2257-earliest-possible-day-of-full-bloom.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int earliestFullBloom(vector<int>& plantTime, vector<int>& growTime) {
+
+
+        vector<pair<int,int>>v;
+        for(int i = 0; i< plantTime.size(); i++)
+        {
+            int first = plantTime[i]; int second = growTime[i];
+            v.push_back({second, first});
+        }
+        sort(v.rbegin(), v.rend());
+       int curDay = 0; 
+        for(int i = 0; i< v.size(); i++)
+        {
+            curDay += v[i].second;
+        }
+        vector<int>vv(v.size(), 0);
+        int dis = v[v.size()-1].second;
+        for(int i = v.size()-2; i>=0; i--)
+        {
+            vv[i] = dis;
+            dis += v[i].second;
+        }
+        int extra = 0; 
+        for(int i = 0; i< v.size(); i++)
+        {
+            int diff = (v[i].first - vv[i]);
+            if(diff>0)
+            {
+                extra = max(diff, extra);
+            }
+        }
+        cout<<curDay<<"\n";
+        cout<<extra<<"\n";
+        return extra + curDay ;
+        
+    }
+};
