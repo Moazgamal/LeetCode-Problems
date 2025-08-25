0ec6e48f3cb5ae914d4c46e6ec35c1d3b8bcdc32
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-08-25 21:46:13 +0300

    Time: 3 ms (60.72%), Space: 22.5 MB (83.58%) - LeetHub

---
+++ 0498-diagonal-traverse/0498-diagonal-traverse.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
+        if(mat.size()==1)
+            return mat[0];
+        // traverse first column
+        bool Flip = false;
+        vector<int> res;
+        int rows = mat.size(); int cols = mat[0].size();
+        int startCol = 0; 
+        for(int r = 0; r< mat.size(); r++)
+        {
+            int row = 0; int c = 0; 
+            
+            row = r; c = 0; 
+            int i = res.size();
+            while(row >= 0 && row<mat.size() && c>=0 && c< mat[0].size())
+            {
+                res.push_back(mat[row][c]);
+                
+                row--; c++;
+            }
+            if(Flip)
+            {
+                int sz = res.size();
+                reverse(res.begin()+i, res.begin()+sz);
+            }
+            Flip = !Flip;
+        }
+        for(int c = 1; c< cols; c++)
+        {
+           
+           int  r = rows-1; int col = c; 
+            int i = res.size();
+            while(r >= 0 && r<mat.size() && col>=0 && col< mat[0].size())
+            {
+                res.push_back(mat[r][col]);
+                
+                r--; col++;
+                
+            }
+            if(Flip)
+            {
+                int sz = res.size();
+                reverse(res.begin()+i, res.begin()+sz);
+            }
+            Flip= !Flip;
+        }
+        return res;
+
+        
+    }
+};
