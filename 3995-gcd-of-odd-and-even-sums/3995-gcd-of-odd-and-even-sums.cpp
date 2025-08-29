7d58fb747f3a4d32bf878ff984cedf7fb3343aee
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-08-29 16:44:46 +0300

    Time: 0 ms (100%), Space: 8.7 MB (11.48%) - LeetHub

--- 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/3995-gcd-of-odd-and-even-sums.cpp
@@ -3,26 +3,11 @@ public:
     int gcdOfOddEvenSums(int n) {
 
         int sumOdd = 0; 
-        int sumEven = 0; int n1 = 0; int n2 = 0; 
-        for(int i =1; true ; i++)
-        {
-            if(i%2 == 0)
-            {
-                sumEven+=i; n1++;
-            }
-            if(n1 == n)
-                break;
-        }
-        for(int i =1; true ; i++)
-        {
-            if(i%2 != 0)
-            {
-                sumOdd+=i; n2++;
-            }
-            if(n2 == n)
-                break;
-        }
-        return __gcd(sumOdd, sumEven);
+        int sumEven = 0;
+        int sum = (n*(n+1))/2;
+        n--;
+        int sum2 = (n*(n+1))/2;
+        return __gcd(sum+sum2, sum*2);
         
     }
 };
