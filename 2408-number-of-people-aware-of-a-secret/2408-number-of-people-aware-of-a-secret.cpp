d66327eb9934f9f99bcc091ce75bd8cedead75f2
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-09-09 18:35:15 +0300

    Time: 57 ms (8.42%), Space: 25.4 MB (5.2%) - LeetHub

---
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    static const int MAX = 2001;  
+    int dp[MAX][MAX];
+    const int MOD = 1e9 + 7;
+
+    int fn(int start, int end, int n, int delay, int forget) {
+       
+        if (dp[start][end] != -1) return dp[start][end];
+
+        long long ways = 0;
+        if (end > n) ways = 1;
+
+        int f = std::min(end - 1, n);
+
+        for (int t = start; t <= f; ++t) 
+        {
+            ways = (ways + fn(t + delay, t + forget, n, delay, forget)) % MOD;
+        }
+
+        dp[start][end] = (int)ways;
+        return dp[start][end];
+    }
+
+public:
+    int peopleAwareOfSecret(int n, int delay, int forget) {
+        memset(dp, -1, sizeof(dp));
+
+        return fn(1 + delay, 1 + forget, n, delay, forget);
+    }
+};
