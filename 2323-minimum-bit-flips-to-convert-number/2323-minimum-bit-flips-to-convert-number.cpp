2429b4ad594c242c36742b586c3caf06dba927d5
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-09-19 05:24:48 +0300

    Time: 0 ms (100%), Space: 8.1 MB (3.72%) - LeetHub

---
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    string conv(int num)
+    {
+        string s = "";
+        while(num > 0)
+        {
+            s.push_back((num%2)+'0');
+            num /= 2;
+        }
+        reverse(s.begin(), s.end());
+        return s; 
+    }
+public:
+    int minBitFlips(int start, int goal) {
+        string str1 = conv(start);
+        string str2 = conv(goal);
+        if(str1.size() > str2.size())
+        {
+            int diff = str1.size() - str2.size(); 
+            string x (diff, '0');
+            str2 = x + str2;
+        }
+        else if(str2.size() > str1.size())
+        {
+            int diff = str2.size() - str1.size(); 
+            string x (diff, '0');
+            str1 = x + str1;
+        }
+        int ans = 0; 
+        for(int i = 0; i< str1.size(); i++)
+        {
+            if(str1[i] != str2[i])
+            {
+                ans++;
+           
+            }
+        }
+        return ans; 
+
+
+        
+    }
+};
