0c439693a2ff5739035f7906a58d87b53ccd5982
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-09-23 07:32:30 +0300

    Time: 368 ms (60.64%), Space: 113.5 MB (58.8%) - LeetHub

--- 3928-split-and-merge-array-transformation/3928-split-and-merge-array-transformation.cpp
+++ 3928-split-and-merge-array-transformation/3928-split-and-merge-array-transformation.cpp
@@ -15,38 +15,50 @@ public:
         if (nums1 == nums2) return 0;
         int n = nums1.size();
 
-        map<vector<int>, int> dist;  // بدل unordered_map + hash
+        map<vector<int>, bool> vis;  
         queue<vector<int>> q;
-
-        dist[nums1] = 0;
+        
         q.push(nums1);
+        int level =1; 
+        while (!q.empty()) 
+        {
+            int sz = q.size();
+            while(sz--)
+            {
+                auto cur = q.front(); q.pop();
+                if(cur == nums2)
+                    return level;
+                vis[cur] = true;
 
-        while (!q.empty()) {
-            auto cur = q.front(); q.pop();
-            int d = dist[cur];
-
-            for (int i = 0; i < n; ++i) {
-                vector<int> v; 
-                for (int j = i; j < n; ++j) {
-                    v.push_back(cur[j]);
-                    vector<int> h;
-                    for (int m = 0; m < n; ++m) {
-                        if (m >= i && m <= j) continue;
-                        h.push_back(cur[m]);
-                    }
-                    for (int k = 0; k <= (int)h.size(); ++k) {
-                        vector<int> nxt = combine(0, k - 1, h, v);
-                        if (nxt == cur) continue;
-                        if (!dist.count(nxt)) {
-                            dist[nxt] = d + 1;
-                            if (nxt == nums2) return d + 1;
-                            q.push(nxt);
+                for (int i = 0; i < n; ++i) 
+                {
+                    vector<int> v; 
+                    for (int j = i; j < n; ++j) 
+                    {
+                        v.push_back(cur[j]);
+                        vector<int> h;
+                        for (int m = 0; m < n; ++m) 
+                        {
+                            if (m >= i && m <= j)
+                                continue;
+                            h.push_back(cur[m]);
+                        }
+                        for (int k = 0; k <= (int)h.size(); ++k) 
+                        {
+                            vector<int> nxt = combine(0, k - 1, h, v);
+                            if (!vis.count(nxt)) 
+                            {
+                                vis[nxt] = true;
+                                if (nxt == nums2) 
+                                    return level;
+                                q.push(nxt);
+                            }
                         }
                     }
                 }
             }
+            level++;
         }
-
         return -1;
     }
 };
