d7f087d86859cd5be896266c5ea364350da7f6b8
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-10-01 02:51:23 +0300

    Time: 11 ms (6.15%), Space: 10.3 MB (41.61%) - LeetHub

--- 4053-majority-frequency-characters/4053-majority-frequency-characters.cpp
+++ 4053-majority-frequency-characters/4053-majority-frequency-characters.cpp
@@ -6,14 +6,14 @@ public:
         map<int,vector<char>, greater<int>>mp2;
         for(int i = 0; i< s.size(); i++)
             mp1[s[i]]++;
-            
+
         int maj = 0; 
-        for(auto &x: mp1)
+        for(auto x: mp1)
         {
             mp2[x.second].push_back(x.first);
             maj = max(maj, (int)mp2[x.second].size());
         }
-        for(auto &x : mp2)
+        for(auto x : mp2)
         {
             if(x.second.size() == maj)
             {
