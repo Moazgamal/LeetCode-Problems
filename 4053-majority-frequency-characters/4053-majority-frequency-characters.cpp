c6638d1517fd0b81a02d989f2f478197c42bd6af
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-10-01 02:51:03 +0300

    Time: 7 ms (33.11%), Space: 10.5 MB (32.64%) - LeetHub

--- 4053-majority-frequency-characters/4053-majority-frequency-characters.cpp
+++ 4053-majority-frequency-characters/4053-majority-frequency-characters.cpp
@@ -5,25 +5,21 @@ public:
         unordered_map<char,int>mp1;
         map<int,vector<char>, greater<int>>mp2;
         for(int i = 0; i< s.size(); i++)
-        {
             mp1[s[i]]++;
-        }
-        int maxi = 0; 
+            
         int maj = 0; 
-        for(auto x: mp1)
+        for(auto &x: mp1)
         {
             mp2[x.second].push_back(x.first);
-            maxi = max(maxi, x.second);
             maj = max(maj, (int)mp2[x.second].size());
         }
-        for(auto x : mp2)
+        for(auto &x : mp2)
         {
             if(x.second.size() == maj)
             {
                 for(auto y : x.second)
-                {
                     ans.push_back(y);
-                }
+
                 return ans;
             }
         }
