6cb906c3ef29f153978bb41bb79270461d9d998d
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-10-06 02:38:38 +0300

    Time: 55 ms (24.21%), Space: 52.1 MB (90.99%) - LeetHub

---
+++ 2600-maximum-tastiness-of-candy-basket/2600-maximum-tastiness-of-candy-basket.cpp
@@ -0,0 +1,38 @@
+class Solution {
+    bool fn(int mid, vector<int>&price, int k)
+    {
+        int last = price[0];
+        k--;
+        for(int i =1; i< price.size(); i++)
+        {
+            if(k == 0)
+                return true; 
+            if(price[i] - last >= mid)
+            {
+                k--; last = price[i];
+            }
+        }
+        return k== 0; 
+    }
+public:
+    int maximumTastiness(vector<int>& price, int k) {
+        sort(price.begin(), price.end());
+        int ans = 0;
+        int start = 0; int end = price[price.size()-1] - price[0];
+        int mid = -1; 
+        while(start <= end)
+        {
+            mid = start + (end-start)/2;
+            if(fn(mid, price, k))
+            {
+                ans = mid;
+                start = mid+1; 
+            }
+            else
+            {
+                end = mid-1; 
+            }
+        }
+        return ans; 
+    }
+};
