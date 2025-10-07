fe38309e35e75817bc0c01a8e2c74d57598f48fa
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-10-07 21:34:51 +0300

    Time: 247 ms (19.67%), Space: 214.7 MB (9.6%) - LeetHub

---
+++ 2568-minimum-fuel-cost-to-report-to-the-capital/2568-minimum-fuel-cost-to-report-to-the-capital.cpp
@@ -0,0 +1,49 @@
+class Solution {
+    pair<long long, int> fn(int node, int parent, unordered_map<int,long long>&mp, int acc,
+    vector<vector<int>>&g, int &seats)
+    {
+        int cars = 1; int pass = 1; 
+        long long cost = 0;
+        for(int i = 0; i< g[node].size(); i++)
+        {
+            if(g[node][i] == parent)
+                continue;
+            mp[g[node][i]] = acc;
+            auto p = fn(g[node][i], node, mp, acc+1, g, seats);
+            cost += p.first;
+            if(p.second == seats)
+                continue;
+            cars++; pass += p.second;
+        }
+        if(node == 0)
+            return {cost, 0};
+        long long fullCars = pass/seats;
+        long long rem = pass%seats;
+        if(cars  == 1)
+        {
+            if(seats == 1)
+                return {cost+mp[node], 1};
+            return {cost+1, 1};
+        }
+        if(fullCars > 0)
+            cost += (fullCars*mp[node]);
+        if(rem > 0)
+            return {cost+1, rem};
+        return {cost, seats};
+    }
+public:
+    long long minimumFuelCost(vector<vector<int>>& roads, int seats) {
+
+        unordered_map<int,long long>mp;
+        int acc = 1;
+        vector<vector<int>>g(roads.size()+2);
+        for(int i = 0; i< roads.size(); i++)
+        {
+            int from = roads[i][0]; int to = roads[i][1];
+            g[from].push_back(to);
+            g[to].push_back(from);
+        }
+        return fn(0, -1, mp, acc, g, seats).first;
+        
+    }
+};
