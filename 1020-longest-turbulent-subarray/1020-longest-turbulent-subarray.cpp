fc7f04cbb1749ccdbed28aa95e066826ae3e8dba
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-10-09 11:51:59 +0300

    Time: 4 ms (33.1%), Space: 44.2 MB (63.12%) - LeetHub

--- 1020-longest-turbulent-subarray/1020-longest-turbulent-subarray.cpp
+++ 1020-longest-turbulent-subarray/1020-longest-turbulent-subarray.cpp
@@ -1,74 +1,48 @@
 class Solution {
+    void fn(vector<int>&arr, int &ans, bool val)
+    {
+        int i = 0; 
+        while(i < arr.size())
+        {
+            int j = i+1; 
+            bool x = i %2 == 0 ? val: !val; 
+            while(j< arr.size())
+            {
+                if(x)
+                {
+                    if(arr[j] > arr[j-1])
+                    {
+                        j++; x = !x; ans = max(ans, j-i); continue;
+                    }
+                    else
+                    {
+                        ans = max(ans, j-i); i = j; break;
+                    }
+                }
+                else
+                {
+                    if(arr[j]< arr[j-1])
+                    {
+                        j++; x= !x; ans = max(ans, j-i); continue;
+                    }
+                    else
+                    {
+                        ans = max(ans, j-i); i = j; break;
+                    }
+                }
+            }
+            if(j == arr.size())
+                return;
+        }
+        return;
+    }
 public:
     int maxTurbulenceSize(vector<int>& arr) {
         int ans = 0;  int i = 0;
         if(arr.size()==1)
             return 1; 
-        while(i < arr.size())
-        {
-            int j = i+1; 
-            bool x = i %2 == 0 ? true: false; 
-            while(j< arr.size())
-            {
-                if(x)
-                {
-                    if(arr[j] > arr[j-1])
-                    {
-                        j++; x = !x; ans = max(ans, j-i); continue;
-                    }
-                    else
-                    {
-                        ans = max(ans, j-i); i = j; break;
-                    }
-                }
-                else
-                {
-                    if(arr[j]< arr[j-1])
-                    {
-                        j++; x= !x; ans = max(ans, j-i); continue;
-                    }
-                    else
-                    {
-                        ans = max(ans, j-i); i = j; break;
-                    }
-                }
-            }
-            if(j == arr.size())
-                break;
-        }
-        i=0;
-        while(i < arr.size())
-        {
-            int j = i+1; 
-            bool x = i %2 == 0 ? false: true; 
-            while(j< arr.size())
-            {
-                if(x)
-                {
-                    if(arr[j] > arr[j-1])
-                    {
-                        j++; x = !x; ans = max(ans, j-i); continue;
-                    }
-                    else
-                    {
-                        ans = max(ans, j-i); i = j; break;
-                    }
-                }
-                else
-                {
-                    if(arr[j]< arr[j-1])
-                    {
-                        j++; x= !x; ans = max(ans, j-i); continue;
-                    }
-                    else
-                    {
-                        ans = max(ans, j-i); i = j; break;
-                    }
-                }
-            }
-            if(j == arr.size())
-                break;
-        }
+        fn(arr, ans, true);
+        fn(arr, ans, false);
         return ans; 
         
     }
