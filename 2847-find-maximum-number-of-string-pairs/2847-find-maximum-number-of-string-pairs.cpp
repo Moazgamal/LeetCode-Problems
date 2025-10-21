75f4370d067d18e4c0b6bd4957ad1257449dc4f3
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-10-21 11:46:42 +0300

    Time: 0 ms (100%), Space: 24.8 MB (80.01%) - LeetHub

--- 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.cpp
+++ 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     int maximumNumberOfStringPairs(vector<string>& words) {
-        unordered_map<int,int>mp;
+        vector<bool>mp(words.size(), false);
         int ans = 0; 
-        for(int i = 0; i< words.size(); i++)
+        int i = 0; int sz = (int)words.size();
+        while(i< sz)
         {
-            if(mp.count(i) !=0)
-                continue;
-            for(int j = i+1; j< words.size(); j++)
+            if(mp[i] == true)
             {
+                i++;
+                continue;
+            }
+            int j = i+1;
+            while(j< sz)
+            {
+                if(mp[j] == true)
+                {
+                    j++; continue;
+                }
                 reverse(words[j].begin(), words[j].end());
                 if(words[i] == words[j])
                 {
-                    mp[j]++; ans++; break;
+                    mp[j]=true; ans++; break;
                 }
                 else
                 {
@@ -21,13 +30,15 @@ public:
                 reverse(words[i].begin(), words[i].end());
                 if(words[i] == words[j])
                 {
-                    mp[j]++; ans++; break;
+                    mp[j]=true; ans++; break;
                 }
                 else
                 {
                     reverse(words[i].begin(), words[i].end());
                 }
+                j++;
             }
+            i++;
         }
         return ans; 
         
