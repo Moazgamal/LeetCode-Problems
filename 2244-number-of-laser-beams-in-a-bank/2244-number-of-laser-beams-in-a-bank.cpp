bc79dc0a0d44288564f463dac7c9d88770b11b6a
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-10-27 12:32:07 +0300

    Time: 10 ms (35.83%), Space: 27.6 MB (39.89%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
+        int sz = bank.size();
         vector<int>rows(bank.size(), 0);
-        for(int i =0; i< bank.size(); i++)
+        for(int i =0; i< sz; i++)
             for(int j = 0; j< bank[i].size(); j++)
                 if(bank[i][j] == '1')
                     rows[i]++;
         
         
-        int ans = 0; 
-        for(int i =0; i< rows.size()-1; )
+        int ans = 0; int i = 0; 
+        while(i< sz-1)
         {
             int j = i+1; 
-            while(j<rows.size() && rows[j] == 0)
+            while(j<sz && rows[j] == 0)
             {
                 j++;
             }
-            if(j == rows.size())
+            if(j == sz)
                 return ans; 
             ans += (rows[i]*rows[j]);
             i=j;
