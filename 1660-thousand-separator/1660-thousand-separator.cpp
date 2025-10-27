13e0ed564c8fad9390510642816e4b1b7f4c53cb
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-10-27 14:22:59 +0300

    Time: 0 ms (100%), Space: 8 MB (14.96%) - LeetHub

--- 1660-thousand-separator/1660-thousand-separator.cpp
+++ 1660-thousand-separator/1660-thousand-separator.cpp
@@ -5,17 +5,15 @@ public:
         if(str.size()<=3)
             return str; 
         string ans = ""; 
-        for(int i =str.size()-3; i>=0; )
+        int i = str.size()-3; 
+        while(i>=0)
         {
            ans  = str.substr(i,3) + ans;
            if(i>0)
                 ans = '.'+ans;
            i-=3;
         }
-        // if(str.size()%3 == 0)
-        //     return ans.substr(1, ans.size()-1); 
         int x = str.size() %3;
-
        return str.substr(0,x)+ans;
         
     }
