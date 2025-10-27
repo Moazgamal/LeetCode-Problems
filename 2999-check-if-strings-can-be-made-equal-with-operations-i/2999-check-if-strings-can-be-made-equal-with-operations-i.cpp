afed61233f94a797752bcd1a7db985e861c86380
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-10-27 16:17:50 +0300

    Time: 0 ms (100%), Space: 8.2 MB (72.84%) - LeetHub

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/2999-check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/2999-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,38 +1,16 @@
 class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
-        vector<int>v1(26,0);
-        vector<int>v2(26,0);
-        int i = 0;
-        while(i<s1.size())
-        {
-            v1[s1[i]-'a']++;
-            v2[s2[i]-'a']++;
-            i++;
-        }
-        i = 0; 
-        while(i<v1.size())
-        {
-            if(v1[i]!=v2[i])
-                return false; 
-            i++;
-        }
-        v1.clear(); v2.clear();
-        // vector<int>v3(26,0);
-        // vector<int>v4(26,0);
-        i = s1.size()-1;
-        v1[s1[i]-'a']++; v1[s1[i-2]-'a']++;
-        v2[s2[i]-'a']++; v2[s2[i-2]-'a']++;
-        if(v1[s1[i]-'a'] != v2[s1[i]-'a'] || v1[s1[i-2]-'a'] != v2[s1[i-2]-'a'])
+        int i = 3;
+        if((s1[i] != s2[i] && s1[i] != s2[i-2]) ||
+        (s1[i-2] != s2[i] && s1[i-2] != s2[i-2]))
             return false;
-        i = i-1;  
-        v1.clear(); v2.clear();
-        // vector<int>v5(26,0);
-        // vector<int>v6(26,0);
-        v1[s1[i]-'a']++; v1[s1[i-2]-'a']++;
-        v2[s2[i]-'a']++; v2[s2[i-2]-'a']++;
-        if(v1[s1[i]-'a'] != v2[s1[i]-'a'] || v1[s1[i-2]-'a'] != v2[s1[i-2]-'a'])
+        i= 2;
+        if((s1[i] != s2[i] && s1[i] != s2[i-2]) ||
+        (s1[i-2] != s2[i] && s1[i-2] != s2[i-2]))
             return false;
         return true; 
+
+        
     }
 };
