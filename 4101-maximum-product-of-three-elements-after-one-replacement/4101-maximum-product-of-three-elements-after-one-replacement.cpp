6c2b955c3b9be65d5132b4891a8813ebe8abaeb9
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-11-02 15:40:12 +0200

    Time: 55 ms (57.14%), Space: 124.3 MB (64.29%) - LeetHub

--- 4101-maximum-product-of-three-elements-after-one-replacement/4101-maximum-product-of-three-elements-after-one-replacement.cpp
+++ 4101-maximum-product-of-three-elements-after-one-replacement/4101-maximum-product-of-three-elements-after-one-replacement.cpp
@@ -2,33 +2,14 @@ class Solution {
 public:
     long long maxProduct(vector<int>& nums) {
         long long ans = INT_MIN;
-
-        sort(nums.begin(), nums.end());
         int sz = nums.size();
-        if(nums[sz-1] >0 && nums[sz-2] >0)
-        {
-            long long acc = (long long)nums[sz-1]*nums[sz-2]*(1e5);
-            ans = max(ans, acc);
-            if(nums[0]<0)
-            {
-                acc = nums[sz-1]*(-1*1e5)*(nums[0]);
-                ans = max(ans, acc);
-            }
-        }
-        else if(nums[sz-1]>0)
-        {
-            long long acc = nums[0]*nums[1]*(1e5);
-            ans = max(ans, acc);
-            acc = nums[0]*(-1*1e5)*nums[sz-1];
-            ans  = max(ans, acc);
-        }
-
-        long long acc = (long long)nums[0]* nums[1] * (1e5);
+        sort(nums.begin(), nums.end());
+        long long acc = (long long)nums[0]*nums[1]*(1e5);
         ans = max(ans, acc);
         acc = nums[0]*(-1*1e5)*nums[sz-1];
         ans = max(ans, acc);
-        acc = (1e5)*nums[sz-1]*nums[sz-2];
-        ans = max(ans, acc); 
+        acc = (long long)nums[sz-1]*nums[sz-2]*(1e5);
+        ans = max(ans, acc);
         return ans;
 
         
