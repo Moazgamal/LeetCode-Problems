99f906a25b92444029ff9459a3d851364abefd70
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-11-02 19:28:37 +0200

    Time: 7 ms (54.57%), Space: 31.6 MB (41.19%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -6,7 +6,7 @@ public:
         int ans = INT_MIN;
         ans = max(ans, nums[0]*nums[1]*nums[nums.size()-1]);
         ans = max(ans, nums[nums.size()-1]*nums[nums.size()-2]*nums[nums.size()-3]);
-        return ans; 
+        return ans;
         
     }
 };
