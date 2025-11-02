912dbaadda46d029d2cb29fc75aa8d0246ca1644
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-11-02 16:07:07 +0200

    Time: 3 ms (100%), Space: 124.4 MB (64.29%) - LeetHub

--- 4101-maximum-product-of-three-elements-after-one-replacement/4101-maximum-product-of-three-elements-after-one-replacement.cpp
+++ 4101-maximum-product-of-three-elements-after-one-replacement/4101-maximum-product-of-three-elements-after-one-replacement.cpp
@@ -3,13 +3,51 @@ public:
     long long maxProduct(vector<int>& nums) {
         long long ans = INT_MIN;
         int sz = nums.size();
-        sort(nums.begin(), nums.end());
-        long long acc = (long long)nums[0]*nums[1]*(1e5);
-        ans = max(ans, acc);
-        acc = nums[0]*(-1*1e5)*nums[sz-1];
-        ans = max(ans, acc);
-        acc = (long long)nums[sz-1]*nums[sz-2]*(1e5);
-        ans = max(ans, acc);
+        long long firstPos = INT_MIN;
+        long long secondPos = INT_MIN;
+        long long firstNeg = INT_MAX;
+        long long secondNeg = INT_MAX;
+        for(int i = 0; i< nums.size(); i++)
+        {
+            if(nums[i]<0)
+            {
+                if(nums[i]< firstNeg)
+                {
+                    secondNeg = firstNeg;
+                    firstNeg = nums[i];
+                }
+                else if(nums[i]<secondNeg)
+                {
+                    secondNeg = nums[i];
+                }
+            }
+            else if(nums[i] > 0)
+            {
+                if(nums[i] > firstPos)
+                {
+                    secondPos = firstPos;
+                    firstPos = nums[i];
+                }
+                else if(nums[i] > secondPos)
+                {
+                    secondPos = nums[i];
+                }
+            }
+        }
+        long long Constant = 1e5;
+        if(firstNeg != INT_MAX && secondNeg != INT_MAX)
+        {
+            ans = max(ans, firstNeg*secondNeg*(Constant));
+            if(firstPos != INT_MIN)
+                ans = max(ans, firstNeg*(-1*Constant)*firstPos);
+        }
+        else if(firstNeg != INT_MAX)
+            ans = max(ans, firstNeg*(-1*Constant)*firstPos);
+            
+        if(firstPos != INT_MIN && secondPos != INT_MIN)
+            ans = max(ans, firstPos*secondPos*(Constant));
+        else
+            ans = max(ans, (long long)0);
         return ans;
 
         
