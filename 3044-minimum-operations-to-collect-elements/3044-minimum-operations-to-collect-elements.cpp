2bf1214f1441aabd9accd4bc0f534771587d8d9c
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-11-05 10:59:42 +0200

    Time: 0 ms (100%), Space: 23 MB (24.61%) - LeetHub

---
+++ 3044-minimum-operations-to-collect-elements/3044-minimum-operations-to-collect-elements.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        unordered_map<int,bool>mp; 
+        int cnt=0;
+        for(int i = nums.size()-1; i >= 0; i--, cnt++)
+        {
+            if(nums[i] <=k)
+                mp[nums[i]]=true;
+                
+            if(mp.size() == k)
+                return cnt+1; 
+        }
+        return cnt;
+        
+    }
+};
