5460184734498355df659ec148069d378f715864
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-11-09 03:05:26 +0200

    Time: 0 ms (100%), Space: 7.9 MB (40.22%) - LeetHub

---
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+
+        int ops = 0; 
+        while(num1 != 0 && num2 != 0)
+        {
+            while(num1>=num2 && num1 != 0 && num2 != 0)
+            {
+                num1-=num2;
+                ops++;
+            }
+            while(num2>num1 && num1 != 0 && num2 != 0)
+            {
+                num2-=num1;
+                ops++;
+            }
+        }
+        return ops;
+        
+    }
+};
