b441985f14e6bb0a160df3a4a7cb2ce2f7a953d4
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-11-22 13:41:17 +0200

    Time: 0 ms (100%), Space: 57.3 MB (72.46%) - LeetHub

---
+++ 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.ts
@@ -0,0 +1,16 @@
+function minimumOperations(nums: number[]): number {
+
+    let ans = 0; 
+        for(let i = 0; i< nums.length; i++)
+        {
+            if(nums[i] % 3 == 0)
+            {
+                continue;
+            }
+            let x1 = Math.floor(nums[i] /3);
+            let c1 = (nums[i]-(x1*3));
+            let c2 = ((3*(x1+1))-nums[i]);
+            ans += Math.min(c1, c2);
+        }return ans; 
+    
+};
