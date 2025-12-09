1c0ab449a8b1977cd03e1a2d23f76c75fe2c9a62
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-12-09 02:35:49 +0200

    Time: 935 ms (30.65%), Space: 445.2 MB (11.68%) - LeetHub

---
+++ 3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int specialTriplets(vector<int>& nums) {
+        const int MOD = 1e9+7;
+        int ans = 0; 
+        unordered_map<int,int>before;
+        unordered_map<int,int>after;
+        vector<int>v1(nums.size());
+        vector<int>v2(nums.size());
+        for(int i = 0; i< nums.size(); i++)
+            {
+                v1[i] = before[nums[i]*2];
+                before[nums[i]]++;
+            }
+        for(int i = nums.size()-1; i>=0; i--)
+            {
+                v2[i] = after[nums[i]*2];
+                after[nums[i]]++;
+            }
+        for(int i = 0; i< v1.size(); i++)
+            {
+                long long f1 = v1[i];
+                long long f2 = v2[i];
+                ans += (f1%MOD*f2%MOD);
+                ans %= MOD;
+            }
+        return ans; 
+        
+        
+        
+    }
+};
