ae4576954ff28afc4d31755ce5d3440500d2579b
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-12-20 20:48:15 +0200

    Time: 10 ms (11.11%), Space: 59.5 MB (38.89%) - LeetHub

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.ts
@@ -0,0 +1,25 @@
+function minDeletionSize(strs: string[]): number {
+
+    let deletedColumns = 0; 
+        let szCol = strs[0].length;
+        let szRow = strs.length;
+        let i  = 0; 
+        while(i< szCol)
+        {
+            let last = strs[0][i];
+            let j = 1; 
+            while(j< szRow)
+            {
+                if(strs[j][i] < last)
+                {
+                    deletedColumns++;
+                    break;
+                }
+                last = strs[j][i];
+                j++;
+            }
+            i++;
+        }
+        return deletedColumns;
+    
+};
