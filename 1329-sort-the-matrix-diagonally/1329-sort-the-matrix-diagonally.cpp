3b09070c3e868a35f373621c9cf473c93a73917e
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2025-12-25 07:46:32 +0200

    Time: 111 ms (6.27%), Space: 13.5 MB (88.75%) - LeetHub

---
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+        for(int r = 0; r< mat.size(); r++)
+        {
+            for(int c = 0; c< mat[0].size(); c++)
+            {
+                cout<<mat[r][c]<<" ";
+            }
+            cout<<"\n";
+        }
+        cout<<"\n";
+        for(int c = 0; c< mat[0].size(); c++)
+        {
+            vector<int>collect;
+            int r = 0; int col = c;
+            while(r>=0 && r< mat.size() && col>=0 && col< mat[0].size())
+            {
+                collect.push_back(mat[r][col]);
+                r++; col++;
+            }
+            r = 0; col = c; 
+            int i = 0; 
+            sort(collect.begin(), collect.end());
+            while(r>=0 && r< mat.size() && col>=0 && col< mat[0].size())
+            {
+                mat[r][col] = collect[i++];
+                r++; col++;
+            }
+        }
+        for(int r = 0; r< mat.size(); r++)
+        {
+            vector<int>collect;
+            int c = 0; int row = r;
+            while(c>=0 && c< mat[0].size() && row>=0 && row< mat.size())
+            {
+                collect.push_back(mat[row][c]);
+                row++; c++;
+            }
+            row = r; c = 0; 
+            int i = 0; 
+            sort(collect.begin(), collect.end());
+            while(row>=0 && row< mat.size() && c>=0 && c< mat[0].size())
+            {
+                mat[row][c] = collect[i++];
+                row++; c++;
+            }
+        }
+        for(int r = 0; r< mat.size(); r++)
+        {
+            for(int c = 0; c< mat[0].size(); c++)
+            {
+                cout<<mat[r][c]<<" ";
+            }
+            cout<<"\n";
+        }
+        return mat;
+
+        
+    }
+};
