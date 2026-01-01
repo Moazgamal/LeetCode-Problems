287122fd0f3e0d8e042ed0080d757fcb40ddea55
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2026-01-02 00:47:21 +0200

    Time: 8 ms (55.95%), Space: 95.8 MB (69.05%) - LeetHub

---
+++ 3788-maximum-score-of-a-split/3788-maximum-score-of-a-split.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public long MaximumScore(int[] nums) {
+
+         long ans = int.MinValue;
+         long coll = 0; 
+        // multiset<int>mt;
+        int[] suffix = new int[nums.Length];
+        Array.Fill(suffix, int.MaxValue);
+        suffix[suffix.Length-1]= nums[nums.Length-1];
+        for(int i = nums.Length-2; i>=0; i--)
+        {
+            suffix[i] = Math.Min(nums[i], suffix[i+1]);
+        }
+        for(int i = 0; i< nums.Length-1; i++)
+        {
+            // mt.erase(mt.find(nums[i]));
+            coll += nums[i];
+            ans = Math.Max(ans, coll-suffix[i+1]);
+        }return ans; 
+        
+    }
+}
