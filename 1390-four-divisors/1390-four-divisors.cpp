8b7521c222cf707a5d6647d6c3715b1fa07096f6
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2026-01-04 13:58:59 +0200

    Time: 720 ms (17.73%), Space: 35.1 MB (12.07%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -26,11 +26,18 @@ class Solution {
 public:
     int sumFourDivisors(vector<int>& nums) {
         int tot = 0; 
+        unordered_map<int,int>mp;
         for(int i = 0; i< nums.size(); i++)
         {
             int sum = 0; 
+            if(mp.count(nums[i]) >0)
+            {
+                tot += mp[nums[i]];
+                continue;
+            }
             getPrimes(nums[i], sum);
             tot+=sum;
+            mp[nums[i]]= sum; 
         }
         return tot; 
     }
