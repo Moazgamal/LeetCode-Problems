c8ff3299b500e322cf38db48cc2b61a3784be487
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2026-01-07 07:07:25 +0200

    Time: 56 ms (14.79%), Space: 107.9 MB (6.34%) - LeetHub

---
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int MOD = 1e9+7;
+    int dfs(TreeNode* &root, unordered_map<TreeNode*, long long>&mp)
+    {
+        if(!root)
+            return 0; 
+        if(!root->right && !root->left)
+        {
+            return  mp[root]= root->val;
+        }
+        int r = dfs(root->right, mp);
+        int l = dfs(root->left, mp);
+        return mp[root]= root->val + r + l;
+    }
+public:
+    int maxProduct(TreeNode* root) {
+        unordered_map<TreeNode*,long long>mp;
+        int totalSum = dfs(root, mp);
+        long long ans = 1; 
+        for(auto &x: mp)
+        {
+            if(x.first == root)
+                continue;
+            ans = max(ans, mp[x.first]*(totalSum-mp[x.first]));
+        }return ans%MOD; 
+
+        
+    }
+};
