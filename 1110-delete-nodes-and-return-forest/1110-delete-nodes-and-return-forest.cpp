522e953dc28c203d9b7bcbfb7fcc5d7f51fb6e73
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2026-01-10 08:10:10 +0200

    Time: 20 ms (24.73%), Space: 26.8 MB (66.63%) - LeetHub

---
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    void dfs(TreeNode* &root, unordered_map<int,bool>&mp,vector<TreeNode*>&ans, TreeNode* parent)
+    {
+        if(!root)
+            return ;
+        
+        if(mp.count(root->val)==0 && !parent)
+            ans.push_back(root);
+        
+        if(root->right)
+        {
+            TreeNode* temp = root->right;
+            if(mp.count(root->right->val)>0)
+            {
+                root->right = nullptr;
+                dfs(temp, mp, ans, nullptr);
+            }
+            else
+            {
+                if(mp.count(root->val) >0)
+                    dfs(temp, mp, ans, nullptr);
+                else
+                    dfs(temp, mp, ans, root);
+            }
+        }
+        if(root->left)
+        {
+            TreeNode* temp = root->left;
+            if(mp.count(root->left->val)>0)
+            {
+                root->left = nullptr;
+                dfs(temp, mp, ans, nullptr);
+            }
+            else
+            {
+                if(mp.count(root->val) >0)
+                    dfs(temp, mp, ans, nullptr);
+                else
+                    dfs(temp, mp, ans, root);
+            }
+        }
+    }
+public:
+    vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        vector<TreeNode*>ans;
+        unordered_map<int,bool>mp; for(auto &x: to_delete) mp[x]=true; TreeNode* parent = nullptr;
+        dfs(root,mp,ans,parent);return ans;
+    }
+};
