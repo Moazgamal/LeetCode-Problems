188a43472b7fbb150d5efbd02619c5ed7bc8ad4c
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2026-01-14 08:37:46 +0200

    Time: 0 ms (100%), Space: 13.6 MB (88.44%) - LeetHub

---
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -0,0 +1,27 @@
+class Solution {
+    int calcDest(int sourceX, int sourceY, int destX, int destY)
+    {
+        if(sourceX == destX)
+            return abs(destY-sourceY);
+        if(sourceY == destY)
+            return abs(destX-sourceX);
+
+        int diffX = abs(sourceX-destX);
+        int diffY = abs(sourceY-destY);
+        if(diffX== diffY)
+            return diffX;
+        if(diffX>diffY)
+            return diffY+(diffX-diffY);
+        return diffX+(diffY-diffX);
+    }
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int ans = 0; 
+        for(int i = 0; i< points.size()-1; i++)
+        {
+            int m = calcDest(points[i][0], points[i][1], points[i+1][0], points[i+1][1]);
+            ans += m; 
+        }
+        return ans;
+    }
+};
