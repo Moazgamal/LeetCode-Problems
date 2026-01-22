a5a0bd16765c2dc9e2a8776d7e8cfb66ee466ce6
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2026-01-22 08:55:23 +0200

    Time: 0 ms (100%), Space: 31.9 MB (69.35%) - LeetHub

--- 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.cpp
+++ 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     int maximizeSquareHoleArea(int n, int m, vector<int>& hBars, vector<int>& vBars) {
-        
         sort(hBars.begin(), hBars.end());
         sort(vBars.begin(), vBars.end());
         int maxi1 = 1; 
         int maxi2 = 1; 
-        for(int i = 0; i< hBars.size(); )
+        int i = 0; int sz1 = (int)hBars.size();
+        int sz2 = (int)vBars.size();
+        while(i< sz1)
         {
             int firstPile = 0; 
+
             if(hBars[i] == 1)
                 firstPile = 1; 
             else
                 firstPile = hBars[i]-1;
+
             int j = i;
-            while(j+1<hBars.size() && hBars[j+1] == hBars[j]+1)
-            {
+
+            while(j+1 < sz1 && hBars[j+1] == hBars[j]+1)
                 j++;
-            }
+
             int secondPile = 0; 
+
             if(hBars[j] == 2+n)
                 secondPile = hBars[j];
             else
@@ -27,19 +31,23 @@ public:
             maxi1 = max(maxi1, secondPile-firstPile);
             i= j+1;
         }
-        for(int i = 0; i< vBars.size(); )
+        i = 0; 
+        while(i < sz2)
         {
             int firstPile = 0; 
+
             if(vBars[i] == 1)
                 firstPile = 1; 
             else
                 firstPile = vBars[i]-1;
+
             int j = i;
-            while(j+1<vBars.size() && vBars[j+1] == vBars[j]+1)
-            {
+
+            while(j+1<sz2 && vBars[j+1] == vBars[j]+1)
                 j++;
-            }
+
             int secondPile = 0; 
+
             if(vBars[j] == 2+m)
                 secondPile = vBars[j];
             else
