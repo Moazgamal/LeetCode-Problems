76687499d9459b2fdaa5496691b56c8243e2c0c5
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2026-01-27 13:20:05 +0200

    Time: 63 ms (80.37%), Space: 173.6 MB (51.18%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,73 @@
+class LRUCache {
+public:
+unordered_map<int,int> mp2;
+unordered_map<int,int> mp3;
+vector<int>v;
+int i = 0; 
+int _capacity = 0; 
+    LRUCache(int capacity) {
+        _capacity = capacity;
+        i=0;
+    }
+    
+    int get(int key) {
+        if(mp2.count(key) > 0)
+        {
+            v.push_back(key);
+            mp3[key] = v.size()-1;
+            return mp2[key];
+        }
+        return -1; 
+    }
+    
+    void put(int key, int value) {
+        if(mp2.size() < _capacity)
+        {
+            if(mp2.count(key) > 0)
+            {
+                mp2[key]=value;
+                v.push_back(key);
+                mp3[key] = v.size()-1;
+            }
+            else
+            {
+                mp2[key]=value;
+                v.push_back(key);
+                mp3[key] = v.size()-1;
+            }
+        }
+        else
+        {
+            if(mp2.count(key) > 0)
+            {
+                mp2[key]=value;
+                v.push_back(key);
+                mp3[key]=v.size()-1;
+            }
+            else
+            {
+                int j = i; 
+                for(; j< v.size(); j++)
+                {
+                    if(mp3[v[j]] == j && mp2.count(v[j])>0)
+                    {
+                        mp2.erase(v[j]);
+                        // mp3.erase(v[j]);
+                        v.push_back(key);
+                        mp3[key]=v.size()-1;
+                        mp2[key]=value;
+                        break;
+                    }
+                }
+                i= j;
+            }
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
