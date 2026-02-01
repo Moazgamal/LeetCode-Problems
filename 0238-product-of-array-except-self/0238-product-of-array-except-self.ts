e5ea239541c90d2d4a087c1533834542895ea85a
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2026-02-01 03:29:33 +0200

    Time: 9 ms (44.07%), Space: 71.5 MB (47.25%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.ts
@@ -0,0 +1,20 @@
+function productExceptSelf(nums: number[]): number[] {
+
+    let result = [];
+        for(let i = 0; i< nums.length; i++)
+            result.push(1);
+        let acc = 1; 
+        for(let i = 0; i< nums.length-1; i++)
+        {
+            result[i+1] = acc*nums[i];
+            acc*=nums[i];
+        }
+        acc =1 ; 
+        for(let i = nums.length-1; i >= 1; i--)
+        {
+            result[i-1] *= (acc*nums[i]);
+            acc*=nums[i];
+        }
+        return result;
+    
+};
