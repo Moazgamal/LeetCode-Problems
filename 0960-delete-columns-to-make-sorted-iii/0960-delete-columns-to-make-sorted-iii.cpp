9e9d44c8ad06ecceff36a7684c6ce94dcb804326
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2026-02-06 00:46:50 +0200

    Time: 184 ms (5.03%), Space: 75.7 MB (6.28%) - LeetHub

---
+++ 0960-delete-columns-to-make-sorted-iii/0960-delete-columns-to-make-sorted-iii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    bool compareStrings(string s1, string s2)
+    {
+        for(int i = 0; i< s2.size(); i++)
+            if(s2[i] < s1[i])
+                return false; 
+        return true; 
+    }
+    int fn(int curIdx, int prev, vector<string>&v, vector<vector<int>>&dp)
+    {
+        if(curIdx >= v.size())
+            return 0; 
+        auto &ret = dp[curIdx][prev];
+        if(ret != -1)
+            return ret; 
+        int c1 = 0; 
+        if(prev == v.size() || compareStrings(v[prev], v[curIdx]))
+            c1 = 1+ fn(curIdx+1, curIdx, v, dp);
+        int c2 = fn(curIdx+1, prev, v, dp);
+
+        return ret = max(c1, c2);
+    }
+public:
+    int minDeletionSize(vector<string>& strs) {
+        vector<string>v;
+        for(int i =0; i< strs[0].size(); i++)
+        {
+            string str = "";
+            for(int j = 0; j< strs.size(); j++)
+                str.push_back(strs[j][i]);
+            cout<<str<<" str\n";
+            v.push_back(str);
+        }
+        vector<vector<int>>dp(v.size()+1, vector<int>(v.size()+1, -1));
+        int jj = fn(0, v.size(),v,dp);
+        cout<<jj<<"\n";
+        return (int)v.size()-fn(0, v.size(),v,dp);
+        
+    }
+};
