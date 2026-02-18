4dfaf04d12a63b718c8387414f9c4e882ee39f0e
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2026-02-18 07:02:36 +0200

    Time: 0 ms (100%), Space: 7.7 MB (62.63%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -1,29 +1,24 @@
 class Solution {
     bool fn(int n)
     {
-        int last = -1;
+        int last = n%2; n/=2;
         while(n>0)
         {
             if(n%2 == 0)
             {
-                if(last == -1)
-                    last = 0;
-                else if(last == 0)
+                if(last == 0)
                     return false; 
-                last = n%2;
+                last = 0;
             }
             else
             {
-                if(last == -1)
-                    last = 1;
-                else if(last == 1)
+                if(last == 1)
                     return false;
-                last = n%2;
+                last = 1;
             }
             n /= 2;
         }
         return true; 
-        
     }
 public:
     bool hasAlternatingBits(int n) {
