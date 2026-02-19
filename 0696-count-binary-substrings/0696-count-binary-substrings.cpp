b3e479b46e70c7d308ed2ea7374fb941fce3cb39
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2026-02-19 11:50:51 +0200

    Time: 4 ms (52.74%), Space: 13 MB (61.91%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.cpp
+++ 0696-count-binary-substrings/0696-count-binary-substrings.cpp
@@ -4,15 +4,17 @@ public:
         int ans = 0; 
         int i = 0; 
         int j = 1;
+        while(j<s.size() && s[j] == s[i])
+            j++;
+        int k = j;
         while(j<s.size())
         {
-            while(j<s.size() && s[j] == s[i])
-                j++;
-            int k = j;
+            k = j;
             while(k<s.size() && s[k] == s[j])
                 k++;
             ans+= (min((j-i), (k-j)));
             i = j;
+            j = k;
         }
         return ans;
     }
