6163268caa4165b944fe880302eb11ae5db3d390
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2026-02-22 20:36:40 +0200

    Time: 0 ms (100%), Space: 8 MB (44.66%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.cpp
@@ -0,0 +1,32 @@
+class Solution {
+    void fn(int &n, int &ans)
+    {
+        int lastPos = -1; 
+        int cnt = -1; 
+        while(n >0)
+        {
+            if(n % 2 == 1)
+            {
+                if(cnt == -1)
+                    cnt = 0; 
+                else
+                {
+                    ans = max(ans, cnt+1); cnt=0;
+                }
+            }
+            else
+            {
+                if(cnt != -1)
+                    cnt++;
+            }
+            n /= 2; 
+        }
+    }
+public:
+    int binaryGap(int n) {
+        int ans = 0; 
+        fn(n, ans);
+        return ans; 
+        
+    }
+};
