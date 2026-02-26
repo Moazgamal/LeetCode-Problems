faf50b564bd3d8798b34f611421b4a993143b1cd
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2026-02-26 23:39:26 +0200

    Time: 8 ms (8.46%), Space: 16.5 MB (5.22%) - LeetHub

---
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -0,0 +1,49 @@
+class Solution {
+    void fn(string &str, int &ans)
+    {
+        string m(str.size()-1, '0');
+        if(m == str.substr(0, str.size()-1))
+            return; 
+        for(int i = str.size()-1; i>=0; i--)
+        {
+            if(str[i] == '1')
+                str[i] = '0';
+            else
+            {
+                str[i] = '1';
+                ans++;
+                break;
+            }
+        }
+    }
+    void fn2(string &str, int &ans)
+    {
+        int i = str.size()-1;
+        for(; i>=0; i--)
+        {
+            if(str[i] == '1')
+                break;
+        }
+        ans += (str.size()-i-1);
+        string s = str.substr(0, i+1);
+        str = "0"+s;
+    }
+public:
+    int numSteps(string s) {
+        int ans = 0;
+        s = "0"+s;
+        while(true)
+        {
+            if(s[s.size()-1] == '1')
+            {
+                int j = ans; 
+                fn(s,ans);
+                if(j == ans)
+                    return ans;
+            }
+            else
+                fn2(s, ans);
+        }
+        return ans; 
+    }
+};
