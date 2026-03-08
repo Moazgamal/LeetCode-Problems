ddcc8de01cc49d7689ef93184476133727cdc1dc
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2026-03-08 03:02:40 +0200

    Time: 11 ms (80.45%), Space: 17.1 MB (65.04%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -3,30 +3,33 @@ public:
     int minFlips(string s) {
         int k = s.size();
         s = s + s;
-        char flag1 = '0',flag2 = '1',st1 = '0',st2 = '1';
-        int left = 0,right = 0,ch1 = 0,ch2 = 0,result = 1e9;
-        while(right < s.size()){
-            if(right-left+1>k){
 
-                if(s[left]!=st1) ch1--;
-                if(s[left]!=st2) ch2--;
-                int matches1 = k-1-ch1;
-                int matches2 = k-1-ch2;
-                ch1 = matches1; ch2 = matches2;
+        int left = 0, ch1 = 0, ch2 = 0, result = 1e9;
+
+        for(int right = 0; right < s.size(); right++){
+
+            char expected1 = (right % 2) ? '1' : '0';
+            char expected2 = (right % 2) ? '0' : '1';
+
+            if(s[right] != expected1) ch1++;
+            if(s[right] != expected2) ch2++;
+
+            if(right - left + 1 > k){
+
+                char leftExp1 = (left % 2) ? '1' : '0';
+                char leftExp2 = (left % 2) ? '0' : '1';
+
+                if(s[left] != leftExp1) ch1--;
+                if(s[left] != leftExp2) ch2--;
+
                 left++;
-                flag1 = (flag1=='0')?'1':'0';
-                flag2 = (flag2=='0')?'1':'0';
             }
-            if(s[right]!=flag1) ch1++;
-            if(s[right]!=flag2) ch2++;
-            flag1 = (flag1=='0')?'1':'0';
-            flag2 = (flag2=='0')?'1':'0';
-            if(right-left+1==k){
-                int diff = min(ch1,ch2);
-                result = min(result,diff);
+
+            if(right - left + 1 == k){
+                result = min(result , min(ch1 , ch2));
             }
-            right++;
-        }        
+        }
+
         return result;
     }
 };
