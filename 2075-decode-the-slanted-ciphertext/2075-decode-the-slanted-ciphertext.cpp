633828a509bb6aee5584356228ab2732d1de4ad0
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2026-04-04 18:19:07 +0200

    Time: 42 ms (40.68%), Space: 47.4 MB (30.51%) - LeetHub

--- 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.cpp
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.cpp
@@ -2,32 +2,25 @@ class Solution {
 public:
     string decodeCiphertext(string encodedText, int rows) {
 
-        int cols  = encodedText.size()/rows;
-       
-        vector<vector<char>>v(rows, vector<char>(cols,' '));
-        int i = 0; 
-        for(int r = 0; r< rows; r++)
-            for(int c = 0; c< cols; c++)
-                v[r][c] = encodedText[i++];
-
-                
+        int cols  = (int)encodedText.size()/rows;
         string ans ="";
-        for(int c = 0; c< cols; c++)
+        int curPos = 0; 
+        int curR = 0; 
+        int total = rows*cols;
+        int i = 0; 
+        while(curPos < cols)
         {
-            int curC = c; int curR= 0; 
-            string str = "";
-            while(curC < cols && curR < rows)
+            while(curPos < total)
             {
-                str.push_back(v[curR][curC]); 
-                curR++; curC++;
+                ans.push_back(encodedText[curPos]);
+                curPos = curPos+cols+1;
             }
-            ans += str;
-            
+            i++;
+            curPos = i; 
         }
         int j = ans.size()-1;
         while(j>=0 && ans[j] == ' ')
             j--;
-
         return ans.substr(0, j+1); 
     }
 };
