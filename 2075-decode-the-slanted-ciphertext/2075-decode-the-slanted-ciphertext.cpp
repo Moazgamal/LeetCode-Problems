eaad443bf5ff28c6efb89b048bc957c5084549b0
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2026-04-04 18:26:45 +0200

    Time: 43 ms (38.14%), Space: 47.4 MB (28.81%) - LeetHub

--- 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.cpp
+++ 2075-decode-the-slanted-ciphertext/2075-decode-the-slanted-ciphertext.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     string decodeCiphertext(string encodedText, int rows) {
-
+        if(encodedText.size()==0)
+            return "";
         int cols  = (int)encodedText.size()/rows;
         string ans ="";
         int curPos = 0; 
         int curR = 0; 
         int total = rows*cols;
         int i = 0; 
-        while(curPos < cols)
+        
+        while(curPos != cols-1)
         {
-            while(curPos < total)
-            {
-                ans.push_back(encodedText[curPos]);
-                curPos = curPos+cols+1;
-            }
-            i++;
-            curPos = i; 
+            ans.push_back(encodedText[curPos]);
+            curPos = curPos+cols+1;
+            if(curPos >= total)
+                curPos = ++i; 
         }
+        ans.push_back(encodedText[cols-1]);
         int j = ans.size()-1;
         while(j>=0 && ans[j] == ' ')
             j--;
