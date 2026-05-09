1f81d1c5b2b886e3f80ad59a9148766f7b7a27dd
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2026-05-09 13:06:09 +0300

    Time: 1735 ms (17.13%), Space: 316.5 MB (57.87%) - LeetHub

---
+++ 2547-minimum-cost-to-split-an-array/2547-minimum-cost-to-split-an-array.cpp
@@ -0,0 +1,44 @@
+class Solution {
+    int fn(int idx, vector<int>&nums, int &k, vector<int> &dp)
+    {
+        if(idx==nums.size())
+            return 0;
+        auto &ret = dp[idx];
+        if(ret != INT_MAX)
+            return ret; 
+        int cost = 0; 
+        unordered_map<int,int>mp;
+        int len=0;
+        for(int i = idx; i< nums.size(); i++)
+        {
+            if(mp.count(nums[i]) == 0)
+            {
+                mp[nums[i]]=1;
+                ret = min(ret, len+k+fn(i+1, nums,k, dp));
+            }
+            else
+            {
+                if(mp[nums[i]]==1)
+                    len+=2;
+                else
+                    len++;
+
+                mp[nums[i]]++;
+                ret = min(ret, len+k+fn(i+1, nums,k, dp));
+            }
+        }
+        return ret;
+    }
+public:
+    int minCost(vector<int>& nums, int k) {
+
+        vector<int>dp(nums.size(),INT_MAX);
+         fn(0, nums, k, dp);
+        for(int i = 0; i < nums.size(); i++)
+        {
+            cout<<dp[i]<<"\n";
+        }
+        return dp[0];
+
+    }
+};
