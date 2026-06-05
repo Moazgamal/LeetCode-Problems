6f4c3536409739e29e9190a5c17257a9f88fc176
Moazgamal <36606886+Moazgamal@users.noreply.github.com>
2026-06-05 08:01:55 +0300

    Time: 32 ms (50.55%), Space: 106.8 MB (51.19%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        sort(asteroids.begin(), asteroids.end());
+        long long m = mass;
+        for(int i = 0; i< asteroids.size(); i++)
+        {
+            if(m >= asteroids[i])
+                m=min((long long)INT_MAX, (long long)m+asteroids[i]);
+            else
+            {
+                return false; 
+            }
+        }
+        return true;
+    }
+};
